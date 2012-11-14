bd488550f38dcf873d6589d5c3d2a667b8d20cb7
Vincent Weyl <vincent.weyl@taiaut.com>
2012-11-14 16:37:48 +0100

    adding progress indicator for picture uploads

--- public/js/src2/brainstorm/whiteboard/wbimport.js
+++ public/js/src2/brainstorm/whiteboard/wbimport.js
@@ -65,7 +65,7 @@ define("Ideafy/Whiteboard/Import", ["Olives/OObject", "Map", "Config", "Olives/M
                             _ctx.clearRect(0,0,_canvas.width, _canvas.height);
                     };
                 
-                _widget.template = '<div class="import"><span class="importbutton"><input type="file" enctype="multipart/form-data" accept = "image/gif, image/jpeg, image/png" data-importevent="listen: touchstart, selectpress; listen:touchend, check; listen: change, preview"><div data-labels="bind:innerHTML, importlbl"></div></span><div id="postpic" class="wbpostit invisible" data-importmodel="bind:setVisibility, content"><div class="postit-cancel" data-importevent="listen:touchstart,cancel"></div><div class="picframe"><canvas id="preview" data-importmodel="bind:showPreview, content"></canvas></div><div name="post" class = "postpostit" data-importevent="listen: touchstart, press; listen:touchend, post"></div><div class = "delpostit" name="del" data-importevent="listen:touchstart, press;listen:touchend, del"></div></div>';
+                _widget.template = '<div class="import"><span class="importbutton"><input type="file" enctype="multipart/form-data" accept = "image/gif, image/jpeg, image/png" data-importevent="listen: touchstart, selectpress; listen:touchend, check; listen: change, preview"><div data-labels="bind:innerHTML, importlbl"></div></span><div id="postpic" class="wbpostit invisible" data-importmodel="bind:setVisibility, content"><div class="postit-cancel" data-importevent="listen:touchstart,cancel"></div><div class="picframe"><canvas id="preview" data-importmodel="bind:showPreview, content"></canvas></div><div name="post" class = "postpostit" data-importevent="listen: touchstart, press; listen:touchend, post"></div><div class = "delpostit" name="del" data-importevent="listen:touchstart, press;listen:touchend, del"></div><div class="uploadprogress" data-importprogress="bind:showProgress, status"></div></div>';
                 // removed <div class="uploadprogress" data-importprogress="bind:innerHTML, status"></div> before the end
                 
                 _widget.plugins.addAll({
@@ -91,7 +91,11 @@ define("Ideafy/Whiteboard/Import", ["Olives/OObject", "Map", "Config", "Olives/M
                                         }       
                                 }
                         }),
-                        "importprogress" : new Model(_progress),
+                        "importprogress" : new Model(_progress, {
+                                "showProgress" : function(status){
+                                        this.innerHTML = status+'%';
+                                }
+                        }),
                         "importevent" : new Event(_widget)        
                 });
                 

52d6726140f05c100f6eebd94e7fda1c8a1dc0da
Vincent Weyl <vincent.weyl@taiaut.com>
2012-11-21 09:56:21 +0100

    fixed ideafy-menu (multiple entries for continue session + wrong style when exiting an ongoing session)

--- public/js/src2/brainstorm/ideafy-menu.js
+++ public/js/src2/brainstorm/ideafy-menu.js
@@ -8,7 +8,7 @@ define("Ideafy/Brainstorm/Menu", ["Olives/OObject", "Map", "Store", "Olives/Mode
                             _user = Config.get("user"),
                             _labels = Config.get("labels"),
                             _menu = new Store(),
-                            _last = {"name": "continue", "active": true, "label": _labels.get("continuesession"), "bg": "continuesession.png", "bgselected":"continueactive.png"},
+                            _last = {"name": "continue", "active": true, "selected": false, "label": _labels.get("continuesession"), "bg": "continuesession.png", "bgselected":"continueactive.png"},
                             _sip = ""; // session in progress
                         
                         // setup     
@@ -17,8 +17,14 @@ define("Ideafy/Brainstorm/Menu", ["Olives/OObject", "Map", "Store", "Olives/Mode
                                         setActive : function(active){
                                                 (active)?this.classList.remove("inactive"):this.classList.add("inactive");
                                         },
-                                        setBg : function(bg){
-                                                this.setAttribute("style", "background-image:url('../img/brainstorm/"+bg+"');")
+                                        setBg : function(selected){
+                                                var id=this.getAttribute("data-ideafymenu_id");
+                                                if (selected){
+                                                        this.setAttribute("style", "background-image:url('../img/brainstorm/"+_menu.get(id).bgselected+"');color:white;")
+                                                }
+                                                else{
+                                                        this.setAttribute("style", "background-image:url('../img/brainstorm/"+_menu.get(id).bg+"');color:#4D4D4D;")        
+                                                }
                                         }
                                 }),
                                 "labels" : new Model(_labels),
@@ -29,20 +35,23 @@ define("Ideafy/Brainstorm/Menu", ["Olives/OObject", "Map", "Store", "Olives/Mode
                         
                         this.press = function(event, node){
                                 var _id = node.getAttribute("data-ideafymenu_id");
-                                node.setAttribute("style", "background-image:url('../img/brainstorm/"+_menu.get(_id).bgselected+"');color:white;")
+                                _menu.update(_id, "selected", true);
                                 node.classList.add("pressed");        
                         };
                         
                         this.start = function(event, node){
                                 var id = node.getAttribute("data-ideafymenu_id");
                                 (id>0) ? $selectScreen(_menu.get(id).name) : $selectScreen(_menu.get(0).name, _sip);
-                                node.classList.remove("pressed");  
+                                node.classList.remove("pressed");
+                                _menu.update(id, "selected", false);
                         };
                         
                         _widget.addContinue = function addContinue(sip){
                                 var arr = JSON.parse(_menu.toJSON());
-                                arr.unshift(_last);
-                                _menu.reset(arr);
+                                if (_menu.get(0).name !== "continue") {
+                                        arr.unshift(_last);
+                                        _menu.reset(arr);
+                                }
                                 _sip = sip;                
                         };
                         
@@ -55,10 +64,10 @@ define("Ideafy/Brainstorm/Menu", ["Olives/OObject", "Map", "Store", "Olives/Mode
                         
                         _widget.reset = function reset(){
                                 _menu.reset([
-                                {"name": "quick", "active": true, "label": _labels.get("quickbmode"), "bg":"quick.png", "bgselected":"quickactive.png"},
-                                {"name": "musession", "active": false, "label": _labels.get("musession"), "bg":"multiuser.png", "bgselected":"multiuseractive.png"},
-                                {"name": "customb", "active": false, "label": _labels.get("customsession"), "bg":"customb.png", "bgselected":"custombactive.png"},
-                                {"name": "tutorial", "active": true, "label": _labels.get("ideafytutorial"), "bg":"tutorial.png", "bgselected":"tutorialactive.png"}       
+                                {"name": "quick", "active": true, "selected": false, "label": _labels.get("quickbmode"), "bg":"quick.png", "bgselected":"quickactive.png"},
+                                {"name": "musession", "active": false, "selected": false, "label": _labels.get("musession"), "bg":"multiuser.png", "bgselected":"multiuseractive.png"},
+                                {"name": "customb", "active": false, "selected": false, "label": _labels.get("customsession"), "bg":"customb.png", "bgselected":"custombactive.png"},
+                                {"name": "tutorial", "active": true, "selected": false, "label": _labels.get("ideafytutorial"), "bg":"tutorial.png", "bgselected":"tutorialactive.png"}       
                                 ]);
                                 // if last session was incomplete add "continue option" to the top of the list
                                 if (_user.get("sessionInProgress") && _user.get("sessionInProgress").id){

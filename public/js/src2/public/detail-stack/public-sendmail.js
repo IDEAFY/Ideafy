559edfd8a9070e5c88204a584e76f1680d4eca5a
Vincent Weyl <vincent.weyl@taiaut.com>
2012-12-04 12:06:20 +0100

    idea sendmail formatting

--- public/js/src2/public/detail-stack/public-sendmail.js
+++ public/js/src2/public/detail-stack/public-sendmail.js
@@ -7,13 +7,13 @@ define("Ideafy/Public/Sendmail", ["Olives/OObject", "Map", "Config", "Olives/Mod
 			    _user = Config.get("user"),
 			    _transport = Config.get("transport"),
 			    _labels = Config.get("labels"),
-			    _mail = new Store({"toField":"", "from": "", "subject":"", "body": "", "attachment": ""});
+			    _mail = new Store({"toField":"", "from": "", "subject":"", "body": "", "signature": "", "attachment": ""});
 		//setup
 		        _widget.plugins.addAll({
                                 "labels": new Model(_labels),
                                 "mail" : new Model(_mail, {
                                         setUserAvatar : function(from){
-                                                this.setAttribute("style", "background: url('"+ Config.get("avatar") + "') no-repeat center center;");
+                                                if (from) this.setAttribute("style", "background: url('"+ Config.get("avatar") + "') no-repeat center center;");
                                         },
                                         date : function date(date){
                                                 if (date) this.innerHTML = Utils.formatDate(date);
@@ -23,6 +23,9 @@ define("Ideafy/Public/Sendmail", ["Olives/OObject", "Map", "Config", "Olives/Mod
                                                     _ui = new Avatar(authors);
                                                 _ui.place(_frag);
                                                 (!this.hasChildNodes())?this.appendChild(_frag):this.replaceChild(_frag, this.firstChild);
+                                        },
+                                        setSignature : function(sign){
+                                                if (!sign) this.setAttribute("placeholder", _user.get("username")+ " <" + _user.get("_id"));     
                                         },
                                         setRating : function(rating){
                                                 
@@ -44,7 +47,7 @@ define("Ideafy/Public/Sendmail", ["Olives/OObject", "Map", "Config", "Olives/Mod
 			_widget.reset = function reset(idea){
 			     console.log(idea);
 			     _error.reset({"errormsg": ""});
-			     _mail.reset({"toField":"", "from": _user.get("username")+" <"+_user.get("_id")+">", "subject":"", "body": "", "attachment": idea});
+			     _mail.reset({"toField":"", "from": _user.get("username")+" <"+_user.get("_id")+">", "subject":"", "body": "", "signature":"", "attachment": idea});
 			     json = {};
 			};
 			
@@ -60,10 +63,13 @@ define("Ideafy/Public/Sendmail", ["Olives/OObject", "Map", "Config", "Olives/Mod
 			_widget.sendMail = function sendMail(){
 			     console.log("send message now"); 
 			     json.type = "doc";
-			     json.from = _mail.get("from");
 			     json.recipient = _mail.get("toField");
-			     json.subject = _mail.get("subject");
+			     json.subject = _user.get("username") + _labels.get("sentdocmsg");
+			     json.header = _mail.get("subject");
 			     json.body = _mail.get("body");
+			     json.signature = _mail.get("from");
+			     json.attachHeader = "<div style='border:1px solid #cccccc'; border-radius=5px; padding = 10px;'>"+ Utils.formatDate(_mail.get("attachment").creation_date) + "<br><p><b>" + mail.get("attachment").title + "</b></p><br><p><i>" + _mail.get("attachment").authornames + "</i></p>";
+			     json.attachBody = "<p style='margin-top: 20px;'>"+_mail.get("attachment").description+"</p><pstyle='margin-top: 20px;'>" + _mail.get("attachment").solution + "</p>";
 			     console.log(json);
 			     _transport.request("SendMail", json, function(result){
 			             if (result.sendmail === "ok"){

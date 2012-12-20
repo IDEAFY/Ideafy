257d9e2b7ff60a2efcab7ed043535befdd54a6cb
Vincent Weyl <vincent.weyl@taiaut.com>
2012-12-20 20:21:21 +0100

    setting up ui for user profile

--- public/js/src2/dashboard/profile/profile.js
+++ public/js/src2/dashboard/profile/profile.js
@@ -19,6 +19,23 @@ define("Ideafy/Dashboard/Profile", ["Olives/OObject", "Map", "Olives/Model-plugi
                                 setAvatar : function(author){
                                                 this.setAttribute("style", "background: url('"+ Config.get("avatar") + "') no-repeat center center;background-size:cover;");
                                 },
+                                setLocation : function(address){
+                                        if (address.country){
+                                                this.innerHTML = address.country.toUpperCase();
+                                        }
+                                        if (address.city && address.country) {
+                                                this.innerHTML=address.city+", "+address.country.toUpperCase();        
+                                        }
+                                        if (address.city && address.state && address.country){
+                                                this.innerHTML=address.city+", "+ address.state.toUpperCase()+" "+address.country.toUpperCase();
+                                        }    
+                                },
+                                setAge : function(dob){
+                                        var now = new Date(), then = new Date(dob[0], dob[1], dob[2]),
+                                            age = now.getTime() - then.getTime();
+                                            
+                                            this.innerHTML = Math.floor(age/1000/3600/24/365);        
+                                },
                                 setFamily : function(family){
                                         var couple = family.couple,
                                             children = family.children,
@@ -53,7 +70,7 @@ define("Ideafy/Dashboard/Profile", ["Olives/OObject", "Map", "Olives/Model-plugi
                                                 this.innerHTML = "";
                                         } 
                                  },
-                                 setInterests : function(interests){
+                                setInterests : function(interests){
                                         var res = "<ul>";
                                         if (interests && interests.length){
                                                 for (i=0; i<interests.length; i++){
@@ -68,7 +85,7 @@ define("Ideafy/Dashboard/Profile", ["Olives/OObject", "Map", "Olives/Model-plugi
                            })
                    });
                    
-                   profileUI.template = '<div id="dashboard-profile"><div class="header blue-dark"><span data-label="bind:innerHTML, profilelbl"></span></div><div id="profile-content"><div class="userdetails"><div class="cd-picarea"><div class="cardpicture" data-user="bind:setAvatar, _id"></div><div class="cardinfo"><p><span class="cd-agelbl"></span><span data-carddetails="bind:innerHTML, age">age</span><span class="agesuffix" data-label="bind:innerHTML, agelbl"></span></p><p><span class="cd-locationlbl"></span><span class="cd-info" data-carddetails="bind: innerHTML, location"></span></p><p><span class="cd-joblbl"></span><span class="cd-info" data-carddetails="bind: innerHTML, occupation.description"></span></p><p><span class="cd-familylbl"></span><span class="cd-info" data-user="bind: setFamily, family"></span></p><p><span class="cd-introlbl"></span><span class="cd-info" data-user="bind:innerHTML, intro"></span></p></div></div><div class="cd-contentarea"><span class="contentTitle" data-label="bind: innerHTML, hobbieslbl">Hobbies</span><p class = "dyknow" data-user="bind:setLeisure, leisure_activities">hobbies</p><span class="contentTitle" data-label="bind: innerHTML, interestslbl">Centers of interest</span><p class = "dyknow" data-user="bind: setInterests, interests">Centers of interest</p></div><div><legend data-stats="bind: setPercentage, completion"></legend><div class="completionbar" data-stats = "bind:setProgress, completion"></div></div></div></div></div>';
+                   profileUI.template = '<div id="dashboard-profile"><div class="header blue-dark"><span data-label="bind:innerHTML, profilelbl"></span></div><div id="profile-content"><div class="leftprofile"><div class="userdetails"><div class="cd-picarea"><div class="cardpicture" data-user="bind:setAvatar, _id"></div><div class="cardinfo"><p><span class="cd-agelbl"></span><span data-user="bind:setAge, birthdate"></span><span class="agesuffix" data-label="bind:innerHTML, agelbl"></span></p><p><span class="cd-locationlbl"></span><span class="cd-info" data-user="bind: setLocation, address"></span></p><p><span class="cd-joblbl"></span><span class="cd-info" data-user="bind: innerHTML, occupation.description"></span></p><p><span class="cd-familylbl"></span><span class="cd-info" data-user="bind: setFamily, family"></span></p><p><span class="cd-introlbl"></span><span class="cd-info" data-user="bind:innerHTML, intro"></span></p></div></div><div class="cd-contentarea"><span class="contentTitle" data-label="bind: innerHTML, hobbieslbl">Hobbies</span><p class = "dyknow" data-user="bind:setLeisure, leisure_activities">hobbies</p><span class="contentTitle" data-label="bind: innerHTML, interestslbl">Centers of interest</span><p class = "dyknow" data-user="bind: setInterests, interests">Centers of interest</p></div><div><legend data-stats="bind: setPercentage, completion"></legend><div class="completionbar"><div class="innerbar" data-stats = "bind:setProgress, completion"></div></div></div></div><div class="edituserdetails invisible"></div><div class="mystats">My stats</div><div class="recenthistory">Recent history</div></div><div class = "rightprofile"><input class="infoslider" type="range"><div class="userscore">User score</div><div class="userachievements">Achievements</div></div></div></div>';
                    
                    profileUI.place(Map.get("dashboard-profile"));
                    

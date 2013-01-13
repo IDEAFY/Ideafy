87b44545577f17397c2536fd91c9a9126fbf9a4f
Vincent Weyl <vincent.weyl@taiaut.com>
2013-01-13 14:59:42 +0100

    carousel for deck presentation

--- public/js/src2/library/decks/deckview/deckdetails.js
+++ public/js/src2/library/decks/deckview/deckdetails.js
@@ -1,15 +1,57 @@
-define("Ideafy/Library/DeckDetails", ["Olives/OObject", "Config", "Olives/Model-plugin", "Olives/Event-plugin", "Store", "Ideafy/Avatar", "Ideafy/Utils"],
-        function(Widget, Config, Model, Event, Store, Avatar, Utils){
+define("Ideafy/Library/DeckDetails", ["Olives/OObject", "Config", "Olives/Model-plugin", "Olives/Event-plugin", "Store", "Ideafy/Avatar", "Ideafy/Utils", "CouchDBStore"],
+        function(Widget, Config, Model, Event, Store, Avatar, Utils, CouchDBStore){
                 
                 return function DeckDetailsConstructor(){
                  
                         var deckDetails = new Widget(),
                             deckModel = new Store(),
-                            allCards = new Store(),
+                            range = new Store({"max": 0}),
+                            deckCards = new Store([]),
+                            allCards = new CouchDBStore(),
                             labels = Config.get("labels");
+                        
+                        
+                        allCards.setTransport(Config.get("transport"));
                         
                         deckDetails.plugins.addAll({
                                 "labels": new Model(labels),
+                                "range": new Model(range, {
+                                        setCursorWidth : function(max){
+                                                console.log(document.querySelector(".deckslider"));        
+                                        }
+                                }),
+                                "cards": new Model(deckCards,{
+                                        setStyle : function(style){
+                                                //used to hide cards when viewing beginning or end of list
+                                                if (style && style === "null"){
+                                                        this.classList.add("invisible");               
+                                                }
+                                                else {
+                                                        this.classList.remove("invisible");
+                                                }
+                                        },
+                                        formatTitle : function(title){
+                                                var id, node = this;
+                                                if (title){
+                                                        id = node.getAttribute("data-cards_id");
+                                                        if (deckCards.get(id).type && deckCards.get(id).type !== 4) {
+                                                                this.innerHTML = title.substring(0,1).toUpperCase()+title.substring(1).toLowerCase(); 
+                                                        }
+                                                        else{
+                                                                this.innerHTML = title.toUpperCase();
+                                                                this.setAttribute("style", "font-family:Helvetica;");
+                                                        }
+                                               }
+                                        },
+                                        setPic : function(pic){
+                                                if (pic){
+                                                        this.setAttribute("style", "background-image:url('"+pic+"');");
+                                                }
+                                                else {
+                                                        this.setAttribute("style", "background-image: none;")
+                                                }
+                                        }
+                                 }),
                                 "deckdetails" : new Model(deckModel, {
                                         formatDate : function(date) {
                                                 (date) ? this.innerHTML = Utils.formatDate(date) : this.innerHTML="";
@@ -30,10 +72,24 @@ define("Ideafy/Library/DeckDetails", ["Olives/OObject", "Config", "Olives/Model-
                                 "carouselevent" : new Event(deckDetails)        
                         });
                         
-                        deckDetails.template = '<div class="deckdetails"><div class="deckinfo"><div class="deckheader"><div class="decklogo" data-deckdetails="bind: setPic, author"></div><p><h2 data-deckdetails="bind:innerHTML, title"></h2><span data-labels="bind:innerHTML, designedby"></span><span data-deckdetails="bind: innerHTML, created_by"></span></p><span class="date" ></span></div><p class="deckdescription" data-deckdetails="bind: innerHTML, description"></p></div><div class="deckcarousel"><div class="innercarousel"></div><ul><li><div class="card"><div class="cardreflection"></div></div></li><li><div class="card"></div><div class="cardreflection"></div></li><li><div class="card"></div><div class="cardreflection"></div></li><li><div class="card"></div><div class="cardreflection"></div></li><li><div class="card"></div><div class="cardreflection"></div></li></ul><input type="range"></div></div>';
+                        deckDetails.template = '<div class="deckdetails"><div class="deckinfo"><div class="deckheader"><div class="decklogo" data-deckdetails="bind: setPic, author"></div><p><h2 data-deckdetails="bind:innerHTML, title"></h2><span data-labels="bind:innerHTML, designedby"></span><span data-deckdetails="bind: innerHTML, created_by"></span></p><span class="date" ></span></div><p class="deckdescription" data-deckdetails="bind: innerHTML, description"></p></div><div class="deckcarousel"><div class="innercarousel"></div><ul data-cards="foreach"><li data-cards="bind: setStyle,style"><div class="card"><div class="cardpicture" data-cards="bind:setPic,picture_file"></div><div class="cardtitle" data-cards="bind: formatTitle, title"></div></div></li></ul><input class="deckslider" type="range" value=0 min=0 data-range="bind: max, max; bind: setCursorWidth, max" data-carouselevent="listen: input, updateCards"></div></div>';
+                        
+                        deckDetails.displayCards = function displayCards(id){
+                                var i, arr = [];
+                                deckCards.reset([]);
+                                for(i=0;i<5;i++){
+                                        (allCards.get(id-2+i))?arr[i]=allCards.get(id-2+i).value : arr[i] = {style: "null"};
+                                }
+                                console.log(arr);
+                                deckCards.reset(arr);
+                        };
+                        
+                        deckDetails.updateCards = function(event, node){
+                                deckDetails.displayCards(node.value);        
+                        };
                         
                         deckDetails.reset = function reset(deck){
-                                var lang = Config.get("user").get("lang"), arr1, arr2, arr3, arr4;
+                                var lang = Config.get("user").get("lang");
                                 // check deck default language -- if it does not match user language look for a translation
                                 if (!deck.default_lang || (deck.default_lang === lang)) {
                                         deckModel.reset(deck);
@@ -41,7 +97,16 @@ define("Ideafy/Library/DeckDetails", ["Olives/OObject", "Config", "Olives/Model-
                                 else {
                                         (deck.translations && deck.translations[lang]) ? deckModel.reset(deck.translations[lang]) : deckModel.reset(deck);
                                 }
-                                // get all cards.     
+                                
+                                //reset card range
+                                range.set("max", 0);
+                                // get all cards.
+                                allCards.unsync();
+                                allCards.sync(Config.get("db"), "library", "_view/cards", {key: '"'+ deckModel.get("_id")+'"'}).then(function(){
+                                        range.set("max", allCards.getNbItems()-1);
+                                        // init card set with three cards
+                                        deckDetails.displayCards(0);   
+                                });
                         };
                         
                         DD= deckDetails;

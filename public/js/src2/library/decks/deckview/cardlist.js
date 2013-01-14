d3370572a098068780c8ee502d5e6976f1e60027
Vincent Weyl <vincent.weyl@taiaut.com>
2013-01-14 11:24:47 +0100

    pagination support in cardlists

--- public/js/src2/library/decks/deckview/cardlist.js
+++ public/js/src2/library/decks/deckview/cardlist.js
@@ -6,11 +6,24 @@ define ("Ideafy/Library/CardList", ["Olives/OObject", "Config", "Olives/Model-pl
                 
                         var cardList = new Widget(),
                             cards = new Store([]),
+                            cardPage = new Store([]),
+                            pagination = new Store({currentPage:0, nbPages: 0}),
+                            touchStart,
                             popupUI,
+                            labels = Config.get("labels"),
                             currentHighlight = null; // used to keep track of current zoom
                         
                         cardList.plugins.addAll({
-                                "cards": new Model(cards, {
+                                "pagination" : new Model(pagination,{
+                                        setPage : function(currentPage){
+                                                var nb = currentPage+1;
+                                                if (pagination.get("nbPages") > 1){
+                                                        this.innerHTML = labels.get("page") + nb + " / " + pagination.get("nbPages");        
+                                                }
+                                                else this.innerHTML = "";
+                                        }
+                                }),
+                                "cards": new Model(cardPage, {
                                         formatTitle : function(title){
                                                 if (title){
                                                         if ($cardType !== "techno") {
@@ -34,7 +47,7 @@ define ("Ideafy/Library/CardList", ["Olives/OObject", "Config", "Olives/Model-pl
                                 "cardlistevent": new Event(cardList)
                         });
                         
-                        cardList.template = '<div class="cardlist"><div id="cardlist-popup" class="invisible"></div><div class="cardpage"><ul data-cards="foreach"><li class="card" data-cardlistevent="listen:touchstart, highlight; listen:touchend, zoom"><div class="cardpicture" data-cards="bind:setPic,picture_file"></div><div class="cardtitle" data-cards="bind: formatTitle, title"></div></li></ul></div></div>';
+                        cardList.template = '<div class="cardlist"><div id="cardlist-popup" class="invisible"></div><div class="cardpage" data-cardlistevent="listen:touchstart, setStart; listen:touchmove, changePage"><span class="pagenb" data-pagination="bind: setPage, currentPage"></span><ul data-cards="foreach"><li class="card" data-cardlistevent="listen:touchstart, highlight; listen:touchend, zoom"><div class="cardpicture" data-cards="bind:setPic,picture_file"></div><div class="cardtitle" data-cards="bind: formatTitle, title"></div></li></ul></div></div>';
                         
                         cardList.reset = function reset(deck){
                                 //reset highlight
@@ -52,8 +65,53 @@ define ("Ideafy/Library/CardList", ["Olives/OObject", "Config", "Olives/Model-pl
                                         cdb.loop(function(v,i){
                                                 cards.alter("push", v.doc);        
                                         });
+                                        // 12 cards max per page
+                                        pagination.set("nbPages", Math.ceil(cards.getNbItems()/12)); 
+                                        
+                                        // display first page
+                                        cardList.displayPage(0);
                                         cdb.unsync();
                                 });
+                        };
+                        
+                        cardList.displayPage = function displayPage(pageNB){
+                                var i, length = cards.getNbItems() - 12 * pageNB; 
+                                
+                                //set page number
+                                pagination.set("currentPage", pageNB);
+                                
+                                // build page content
+                                cardPage.reset([]);
+                                for (i = 0; i< length; i++){
+                                        cardPage.alter("push", cards.get(pageNB*12+i));        
+                                }
+                        };
+                        
+                        cardList.displayPrevious = function displayPrevious(){
+                                var nb = pagination.get("currentPage");
+                                if (nb>0) cardList.displayPage(nb-1)        
+                        };
+                        
+                        cardList.displayNext = function displayNext(){
+                                var nb = pagination.get("currentPage");
+                                if (nb<(pagination.get("nbPages")-1)) cardList.displayPage(nb+1);        
+                        };
+                        
+                        cardList.setStart = function(event, node){
+                                touchStart = [event.pageX, event.pageY];
+                        };
+                
+                        cardList.changePage = function(event, node){
+                                touchPoint = [event.pageX, event.pageY];
+                                
+                                if ((touchStart[0]-touchPoint[0]) > 40 && (touchPoint[1]-touchStart[1])<20 && (touchPoint[1]-touchStart[1])>-20){
+                                        console.log("displaynext");
+                                        cardList.displayNext();
+                                }
+                                else if ((touchStart[0]-touchPoint[0]) < 40 && (touchPoint[1]-touchStart[1])<20 && (touchPoint[1]-touchStart[1])>-20){
+                                        console.log("displayprev");
+                                        cardList.displayPrevious();
+                                }
                         };
                         
                         cardList.highlight = function(event, node){

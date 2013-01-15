b2a4cad389310eee8deba3b256a37b7acbed749e
Vincent Weyl <vincent.weyl@taiaut.com>
2013-01-15 14:08:51 +0100

    support form

--- public/js/src2/dashboard/about/support.js
+++ public/js/src2/dashboard/about/support.js
@@ -5,14 +5,21 @@
  * Copyright (c) 2012-2013 TAIAUT
  */
 
-define("Ideafy/Dashboard/Support", ["Olives/OObject", "Config", "Olives/Model-plugin", "Olives/Event-plugin"],
-        function(Widget, Config, Model, Event){
+define("Ideafy/Dashboard/Support", ["Olives/OObject", "Config", "Olives/Model-plugin", "Olives/Event-plugin", "Store"],
+        function(Widget, Config, Model, Event, Store){
                 return function SupportConstructor(){
                   
                         var support = new Widget(),
-                            labels = Config.get("labels");
+                            labels = Config.get("labels"),
+                            model = new Store();
                         
-                        support.template = '<div class="aboutcontent">Support<textarea class="input"></textarea></div>';
+                        support.plugins.addAll({
+                                "labels": new Model(labels),
+                                "support": new Model(model),
+                                "supportevent": new Event(support)
+                        });
+                        
+                        support.template = '<div class="aboutcontent"><legend class"support" data-labels="bind:innerHTML, supportlegend"></legend><textarea class="input" data-labels="bind:placeholder, supportplaceholder"></textarea><div class="cancel" data-labels="bind: innerHTML, cancellbl" data-supportevent="listen: touchstart, press; listen: touchend, cancel"></div><div class="send" data-labels="bind: innerHTML, sendlbl" data-twocentevent="listen: touchstart, press; listen: touchend, send"></div></div>';
                         
                         return support;      
                         

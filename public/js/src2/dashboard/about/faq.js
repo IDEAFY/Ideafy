8faf6eecb8e9fab875cbb39b0f5ea021d73a5373
Vincent Weyl <vincent.weyl@taiaut.com>
2013-01-26 16:41:48 +0100

    added faq mechanism

--- public/js/src2/dashboard/about/faq.js
+++ public/js/src2/dashboard/about/faq.js
@@ -5,14 +5,33 @@
  * Copyright (c) 2012-2013 TAIAUT
  */
 
-define("Ideafy/Dashboard/FAQ", ["Olives/OObject", "Config"],
-        function(Widget, Config){
+define("Ideafy/Dashboard/FAQ", ["Olives/OObject", "Config", "CouchDBStore", "Olives/Model-plugin", "Olives/Event-plugin", "Store"],
+        function(Widget, Config, CouchDBStore, Model, Event, Store){
            
            return function FAQConstructor(){
                 
-                var FAQ = new Widget();
+                var FAQ = new Widget(),
+                    cdb = new CouchDBStore(),
+                    lang = Config.get("user").get("lang");
+                    faqlist = new Store([]);
+                    
+                FAQ.plugins.addAll({
+                        "faq" : new Model(faqlist),
+                        "faqevent" : new Event(FAQ)
+                });
                 
-                FAQ.template = '<div class="aboutcontent"><Frequently asked questions/div>';
+                FAQ.template = '<div class="aboutcontent"><ul data-faq="foreach"><li><legend data-faq="bind:innerHTML, question"></legend><p data-faq="bind: innerHTML, response"></p></li></ul></div>';
+                
+                // fetch faqs from database
+                cdb.setTransport(Config.get('transport'));
+                
+                cdb.sync(Config.get("db"), "about", "_view/faq").then(function(){
+                        cdb.loop(function(v,i){
+                                console.log(v);
+                                if (v.value.default_lang === lang || !v.value.translations[lang]) faqlist.alter("push", v.value)
+                                else faqlist.alter("push", v.value.translations[lang]);
+                        });      
+                });
                 
                 return FAQ;         
            };   

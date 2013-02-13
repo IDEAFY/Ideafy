07e233b92443134889b2cd5bd000f3c01e2f8409
Vincent Weyl <vincent.weyl@taiaut.com>
2013-02-13 14:59:38 +0100

    adding eula to application

--- public/js/src2/dashboard/about/eula.js
+++ public/js/src2/dashboard/about/eula.js
@@ -5,14 +5,42 @@
  * Copyright (c) 2012-2013 TAIAUT
  */
 
-define("Ideafy/Dashboard/EULA", ["Olives/OObject", "Config"],
-        function(Widget, Config){
+define("Ideafy/Dashboard/EULA", ["Olives/OObject", "Config", "Store", "CouchDBStore", "Olives/Model-plugin"],
+        function(Widget, Config, Store, CouchDBStore, Model){
            
                 return function EULAConstructor(){
                         
-                        var eula = new Widget();
+                        var eula = new Widget(),
+                            user = Config.get("user"),
+                            model = new Store();
+                            
+                        eula.plugins.add("eula", new Model(model))
                         
-                        eula.template = '<div class="aboutcontent">End-user license agreement</div>';
+                        eula.template = '<div class="aboutcontent"><h4 data-eula = "bind:innerHTML, title"></h4><div data-eula="bind:innerHTML, body"></div></div>';
+                        
+                        // fetch faqs from database
+                        eula.fetch = function fetch(lang){
+                                var cdb = new CouchDBstore();
+                                cdb.setTransport(Config.get("transport"));
+                                cdb.sync(Config.get("db"), "EULA").then(function(){
+                                        if (cdb.get("default_lang") === lang || !cdb.get("translations")[lang]){
+                                                model.set("title", cdb.get("title"));
+                                                model.set("eula", cdb.get("body"));
+                                        }
+                                       else {
+                                               model.set("title", cdb.get("translations")[lang].title);
+                                               model.set("body", cdb.get("translations")[lang].body);
+                                        }      
+                                });
+                        };
+                
+                        // init
+                        eula.fetch(user.get("lang"));
+                
+                        // watch for language change
+                        user.watchValue("lang", function(){
+                                eula.fetch(user.get("lang"));                
+                        });
                         
                         return eula;                
                 };

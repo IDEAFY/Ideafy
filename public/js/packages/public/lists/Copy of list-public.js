67414a80b18dfdfca7cdf40b45b2f7b80487d0c6
Vincent Weyl <vincent.weyl@taiaut.com>
2013-02-14 11:37:01 +0100

    reverting last changes

--- public/js/packages/public/lists/Copy of list-public.js
+++ public/js/packages/public/lists/Copy of list-public.js
@@ -6,7 +6,7 @@
  */
 
 define(["Olives/OObject", "CouchDBStore", "service/config", "Olives/Model-plugin", "Olives/Event-plugin", "service/utils", "service/avatar", "service/actionbar", "Promise"], function(Widget, Store, Config, Model, Event, Utils, Avatar, ActionBar, Promise) {
-        function ListPublicConstructor($db, $design, $view, $query, $type) {
+        function ListPublicConstructor($db, $design, $view, $query) {
                 var _store = new Store([]),
                 touchStart,
                 touchPoint,
@@ -23,7 +23,8 @@ define(["Olives/OObject", "CouchDBStore", "service/config", "Olives/Model-plugin
                         }
                 };
 
-                
+                //setup
+                _store.setTransport(Config.get("transport"));
                 this.template = "<ul class='ideas-list' data-listideas='foreach'>" + "<li class='list-item' data-listevent='listen:touchstart, setStart; listen:touchmove, showActionBar'>" + "<div class='item-header'>" + "<div class='avatar' data-listideas='bind:setAvatar,doc.authors'></div>" + "<h2 data-listideas='bind:innerHTML,doc.authornames'></h2>" + "<span class='date' data-listideas='bind:date,doc.creation_date'></span>" + "</div>" + "<div class='item-body'>" + "<h3 data-listideas='bind:innerHTML,doc.title'>Idea title</h3>" + "<p data-listideas='bind:innerHTML,doc.description'></p>" + "</div>" + "<div class='item-footer'>" + "<a class='idea-type'></a>" + "<a class='item-acorn'></a>" + "<span class='rating' data-listideas='bind:setRating, value.rating'></span>" + " </div>" + "</li>" + "</ul>";
 
                 this.plugins.addAll({
@@ -62,17 +63,11 @@ define(["Olives/OObject", "CouchDBStore", "service/config", "Olives/Model-plugin
                         var promise=new Promise();
                         _options.query = query;
 
-                        
-                        if ($type === "polling"){
-                                        
-                        }
-                        else{
-                                _store.unsync();
-                                _store.reset([]);
-                                _store.sync(_options.db, _options.design, _options.view, _options.query).then(function(){
-                                        promise.resolve();
-                                });
-                        }
+                        _store.unsync();
+                        _store.reset([]);
+                        _store.sync(_options.db, _options.design, _options.view, _options.query).then(function(){
+                                promise.resolve();
+                        });
                         return promise;
                 };
                 
@@ -113,43 +108,18 @@ define(["Olives/OObject", "CouchDBStore", "service/config", "Olives/Model-plugin
                         _options.query = $query;
                 }
                 
-                // a function to poll fresh data from the database
-                this.refreshList = function refreshList(){
-                        var cdb = new Store();
-                        cdb.setTransport(Config.get("transport"));
-                        cdb.sync(_options.db, _options.design, _options.view, _options.query).then(function(){
-                                        console.log("called every 60 seconds");
-                                        _store.reset(JSON.parse(cdb.toJSON()));     
-                        });        
-                };
-                
                 this.init = function init(initCallback){
-                        var promise = new Promise(),
-                            cdb = new Store();
-                        if ($type === "polling"){
-                                console.log("init list date");
-                                cdb.setTransport(Config.get("transport"));
-                                cdb.sync(_options.db, _options.design, _options.view, _options.query).then(function(){
-                                        console.log(cdb.toJSON());
-                                        _store.reset(JSON.parse(cdb.toJSON()));
-                                        initCallback(_store, 0);
-                                        setTimeout(function(){refreshList();}, Config.get("polling_interval"));
-                                        promise.resolve();     
-                                });                
-                        }
-                        else{
-                                _store.setTransport(Config.get("transport"));
-                                _store.sync(_options.db, _options.design, _options.view, _options.query).then(function(){
-                                        initCallback(_store, 0);
-                                        promise.resolve();      
-                                });
-                        }
+                        var promise = new Promise();
+                        _store.sync(_options.db, _options.design, _options.view, _options.query).then(function(){
+                                initCallback(_store, 0);
+                                promise.resolve();      
+                        });
                         return promise;
                 };
 
         }
 
-        return function ListPublicFactory($db, $design, $view, $query, $type) {
+        return function ListPublicFactory($db, $design, $view, $query) {
                 ListPublicConstructor.prototype = new Widget();
                 return new ListPublicConstructor($db, $design, $view, $query);
         };

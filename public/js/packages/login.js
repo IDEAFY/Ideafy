662abefb52d407c9873db5c0a10b4e5549487ed0
Vincent Weyl <vincent.weyl@taiaut.com>
2013-02-18 05:09:34 +0100

    .

--- public/js/packages/login.js
+++ public/js/packages/login.js
@@ -16,22 +16,19 @@ define(["Olives/OObject" ,"Amy/Stack-plugin",
 				_loading = new Widget(),
 				_serverdown = new Widget(),
 				_stack = new Stack();
-		        
-		        _login.plugins.addAll({
+		//setup
+			_loginForm.alive(Map.get("login-form"));
+			_signupForm.alive(Map.get("signup-form"));
+			_loading.alive(Map.get("loading"));
+			_serverdown.alive(Map.get("serverdown"));
+
+			_login.plugins.addAll({
 				"loginstack" : _stack,
 				"label": new Model(Config.get("labels")),
 				"loginevent" : new Event(this)
 			});
 			
-			_login.template = '<div id="login-stack" data-loginstack="destination"><form id="login-form" data-event="selector:input,keypress,resetError;selector:.login-button,touchend,login"><p class="login-fields"><input data-loginmodel="bind:value,email" autofocus="autofocus" data-label="bind:placeholder, emailplaceholder" type="text"><input data-loginmodel="bind:value,password" type="password" data-label="bind:placeholder, passwordplaceholder" data-event="listen:keypress, enterlogin"></p><p><label class="login-button pressed-btn" data-label="bind: innerHTML, loginbutton"></label></p><p><label data-loginmodel="bind:innerHTML,error" class="login-error"></label></p><!-- may be set a instead label --><p><label id="signup-button" class="pressed-btn" name="#signup-screen" data-label="bind: innerHTML, newuserbutton" data-loginstack="show:touchstart,name"></label></p></form><form id="signup-form" data-event="selector:input,keypress,resetError;selector:#signup,touchend,signup"><p class="login-fields"><input data-loginmodel="bind:value,email" data-label="bind:placeholder, emailplaceholder" type="text"><input data-loginmodel="bind:value,password" type="password" data-label="bind:placeholder, passwordplaceholder"><input data-loginmodel="bind:value,confirm-password" type="password" data-label="bind:placeholder, repeatpasswordplaceholder"><input data-loginmodel="bind:value,firstname" type="text" data-label="bind:placeholder, firstnameplaceholder"><input data-loginmodel="bind:value,lastname" type="text" data-label="bind:placeholder, lastnameplaceholder" data-event="listen:keypress, entersignup"></p><p><label data-loginmodel="bind:innerHTML,error" class="login-error"></label></p><p><label id="signup" class="login-button pressed-btn" data-label="bind:innerHTML, signupbutton"></label></p><p><label class="login-button pressed-btn" name="#login-screen" data-loginstack="show:touchstart,name" data-label="bind:innerHTML, loginbutton"></label></p></form><div id="loading" data-label="bind: innerHTML, loadingmessage">Loading...</div><div id="serverdown" data-label="bind: innerHTML, maintenancemessage">Server down for maintenance</div></div>';
-			
-			_login.place(Map.get("login"));
-			
-			_loginForm.alive(Map.get("login-form"));
-                        _signupForm.alive(Map.get("signup-form"));
-                        _loading.alive(Map.get("loading"));
-                        _serverdown.alive(Map.get("serverdown"));
-			
+			_login.alive(Map.get("login"));
 			_stack.getStack().add("#login-screen", _loginForm);
 			_stack.getStack().add("#signup-screen", _signupForm);
 			_stack.getStack().add("#loading-screen", _loading);

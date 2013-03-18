76027d558966f7728aa1ad5cf466289ead1fe28c
Vincent Weyl <vincent.weyl@taiaut.com>
2013-03-18 10:53:04 +0100

    .

--- public/js-touch/packages/brainstorm/brainstorm.js
+++ public/js-touch/packages/brainstorm/brainstorm.js
@@ -67,7 +67,7 @@ define(["Olives/OObject", "service/map", "service/submenu", "Amy/Stack-plugin", 
                                                         _stack.getStack().add("quick", new QuickB(sip, _widget.exitSession));
                                                         break;
                                                  case "musession":
-                                                        _stack.getStack().add("multi", new MultiB(sip, _widget.exitSession));
+                                                        _stack.getStack().add("musession", new MultiB(sip, _widget.exitSession));
                                                         break;
                                                 case "tutorial":
                                                         Config.get("observer").notify("display-tutorials");

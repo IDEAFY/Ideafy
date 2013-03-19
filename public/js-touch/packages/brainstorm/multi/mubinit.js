3fa0b1ebc449c33901fd75d740855096fe989d82
Vincent Weyl <vincent.weyl@taiaut.com>
2013-03-19 06:13:02 +0100

    .

--- public/js-touch/packages/brainstorm/multi/mubinit.js
+++ public/js-touch/packages/brainstorm/multi/mubinit.js
@@ -11,14 +11,16 @@ define(["Olives/OObject", "Amy/Stack-plugin", "Olives/Model-plugin", "Olives/Eve
            return function MultiBInitConstructor($exit){
            
                 var widget = new Widget(),
-                    stack = new Stack();
+                    stack = new Stack(),
+                    labels = Config.get("labels");
                     
                 widget.plugins.addAll({
+                        "labels": new Model(labels),
                         "muinitstack": stack,
                         "muinitevent": new Event(widget)
                 });
                 
-                widget.template = '<div id="mub-init"><input id="muinitslider" type="range" min="0" max="1" value ="1" data-muinitevent="listen: touchend, toggleMode"><div class="stack" data-muinitstack="destination"></div></div>';
+                widget.template = '<div id="mub-init"><div id="muinitsliderlbl"></div><input id="muinitslider" type="range" min="0" max="1" value ="1" data-muinitevent="listen: touchend, toggleMode"><div class="stack" data-muinitstack="destination"></div></div>';
                 
                 widget.place(document.getElementById("mub-init"));
                 

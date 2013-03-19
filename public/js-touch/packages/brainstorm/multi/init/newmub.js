94566ec475dfbed0c2a4c39050882eac1142cfe1
Vincent Weyl <vincent.weyl@taiaut.com>
2013-03-19 14:32:03 +0100

    .

--- public/js-touch/packages/brainstorm/multi/init/newmub.js
+++ public/js-touch/packages/brainstorm/multi/init/newmub.js
@@ -11,14 +11,45 @@ define(["Olives/OObject", "Olives/Model-plugin", "Olives/Event-plugin", "CouchDB
            return function NewMUBConstructor($exit){
            
                 var widget = new Widget(),
+                    sessionTemplate = {"title" : "",
+                                "description" : "",
+                                "initiator" : {
+                                        "id" : "",
+                                        "username" : "",
+                                        "picture_file" : ""
+                                },
+                                "participants" : [],
+                                "date" : [],
+                                "startTime" : null,
+                                "resumeTime" : null,
+                                "duration" : null,
+                                "elapsedTime" : 0,
+                                "elapsedTimers" : {},
+                                "mode" : "",
+                                "type" : 8,
+                                "deck" : "",
+                                "status" : "waiting",
+                                "step" : "",
+                                "lang" : "en-us",
+                                "characters" : [],
+                                "contexts" : [],
+                                "problems" : [],
+                                "scenarioWB" : [],
+                                "scenario" : [], //{"title" : "", "story" : "", "solution" : ""}
+                                "techno" : [[]],
+                                "ideaWB" : [],
+                                "idea" : [], //{"title" : "", "description" : "", "solution" : "", "visibility" : "private", "id" : "" }
+                                "score" : "",
+                                "chat" : []},
                     labels= Config.get("labels"),
                     user = Config.get("user");
                 
                 widget.plugins.addAll({
-                        "labels": new Model(labels)
+                        "labels": new Model(labels),
+                        "model": new Model()
                 });
                 
-                widget.template = '<div id="newmub"><form><legend>Session settings</legend><label data-labels="bind:innerHTML, quickstarttitle"></label><hr/><textarea class="quickstart-title" name="title" data-model="bind:value, title; bind: setTitle, initiator"></textarea><label data-labels="bind:innerHTML, quickstartdesc"></label><hr/><textarea class="quickstart-desc" name="description" data-model="bind:value, description" data-labels="bind: placeholder, quickstartdescplaceholder"></textarea></form></div>';
+                widget.template = '<div id="newmub"><form><legend>Session settings</legend><label>Select mode</label><select><option>Roulette</option><option>Campfire</option><option>Boardroom</option></select><span class="session-info">What does it mean?</span><label>Choose language</label><label data-labels="bind:innerHTML, quickstarttitle"></label><hr/><textarea class="session-title" name="title" data-model="bind:value, title; bind: setTitle, initiator"></textarea><label data-labels="bind:innerHTML, quickstartdesc"></label><hr/><textarea class="session-desc" name="description" data-model="bind:value, description" data-labels="bind: placeholder, quickstartdescplaceholder"></textarea></form></div>';
                 
                 widget.place(document.getElementById("newmub"));
                 

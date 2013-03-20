8ca8479376218203d7cdc68c617a63ed0e968d99
Vincent Weyl <vincent.weyl@taiaut.com>
2013-03-20 08:14:25 +0100

    .

--- public/js-touch/packages/brainstorm/multi/init/newmub.js
+++ public/js-touch/packages/brainstorm/multi/init/newmub.js
@@ -11,12 +11,43 @@ define(["Olives/OObject", "Olives/Model-plugin", "Olives/Event-plugin", "CouchDB
            return function NewMUBConstructor($exit){
            
                 var widget = new Widget(),
-                    sessionTemplate = {"title" : "",
+                    session = new Store({}),
+                    error = new Store({"errormsg":""}),
+                    labels= Config.get("labels"),
+                    user = Config.get("user");
+                
+                widget.plugins.addAll({
+                        "labels": new Model(labels),
+                        "newmub": new Model(session, {
+                                setSessionInfo : function(mode){
+                                        switch(mode){
+                                                case "campfire":
+                                                        this.innerHTML = labels.get("campfireinfo");
+                                                        break;
+                                                case "boardroom":
+                                                        this.innerHTML = labels.get("boardroominfo");
+                                                        break;
+                                                default:
+                                                        this.innerHTML = labels.get("rouletteinfo");
+                                                        break;
+                                        }
+                                }
+                        }),
+                        "errormsg": new Model(error),
+                        "newmubevent": new Event(widget)
+                });
+                
+                widget.template = '<div id="newmub"><form><label>Select mode</label><hr/><div class="select-mode"><select><option name="roulette" data-labels="bind:innerHTML, roulette"></option><option name="campfire" data-labels="bind:innerHTML, campfire"></option><option name="boardroom" data-labels="bind:innerHTML, boardroom"></option></select><span class="session-info" data-newmub="bind: setSessionInfo, mode">What does it mean?</span></div><div class="invite-contacts invisible"><label>Invite your contacts</label><hr/></div><label data-labels="bind:innerHTML, quickstarttitle"></label><hr/><textarea class="session-title" name="title" data-newmub="bind:value, title; bind: setTitle, initiator"></textarea><label data-labels="bind:innerHTML, quickstartdesc"></label><hr/><textarea class="session-desc" name="description" data-model="bind:value, description" data-labels="bind: placeholder, quickstartdescplaceholder"></textarea></form><div class="newmub-footer"><p class="send"><label class="clear" data-labels="bind:innerHTML, cancellbl" data-newmubevent="listen: touchstart, press; listen:touchend, clear">Clear</label><label class="create" data-labels="bind:innerHTML, sendlbl" data-newmubevent="listen:touchstart, press; listen:touchend, create">Create</label><label class="editerror" data-errormsg="bind:innerHTML, errormsg"></label></p></div></div>';
+                
+                widget.place(document.getElementById("newmub"));
+                
+                widget.reset = function reset(){
+                        var sessionTemplate = {"title" : "",
                                 "description" : "",
                                 "initiator" : {
-                                        "id" : "",
-                                        "username" : "",
-                                        "picture_file" : ""
+                                        "id" : user.get("_id"),
+                                        "username" : user.get("username"),
+                                        "picture_file" : user.get("picture_file")
                                 },
                                 "participants" : [],
                                 "date" : [],
@@ -25,12 +56,12 @@ define(["Olives/OObject", "Olives/Model-plugin", "Olives/Event-plugin", "CouchDB
                                 "duration" : null,
                                 "elapsedTime" : 0,
                                 "elapsedTimers" : {},
-                                "mode" : "",
+                                "mode" : "roulette",
                                 "type" : 8,
                                 "deck" : "",
                                 "status" : "waiting",
                                 "step" : "",
-                                "lang" : "en-us",
+                                "lang" : user.get("lang"),
                                 "characters" : [],
                                 "contexts" : [],
                                 "problems" : [],
@@ -40,24 +71,8 @@ define(["Olives/OObject", "Olives/Model-plugin", "Olives/Event-plugin", "CouchDB
                                 "ideaWB" : [],
                                 "idea" : [], //{"title" : "", "description" : "", "solution" : "", "visibility" : "private", "id" : "" }
                                 "score" : "",
-                                "chat" : []},
-                    session = new Store({}),
-                    error = new Store({"errormsg":""}),
-                    labels= Config.get("labels"),
-                    user = Config.get("user");
-                
-                widget.plugins.addAll({
-                        "labels": new Model(labels),
-                        "model": new Model(session),
-                        "errormsg": new Model(error),
-                        "newmubevent": new Event(widget)
-                });
-                
-                widget.template = '<div id="newmub"><form><label>Select mode</label><hr/><div class="select-mode"><select><option>Roulette</option><option>Campfire</option><option>Boardroom</option></select><span class="session-info">What does it mean?</span></div><div class="invite-contacts invisible"><label>Invite your contacts</label><hr/></div><label data-labels="bind:innerHTML, quickstarttitle"></label><hr/><textarea class="session-title" name="title" data-model="bind:value, title; bind: setTitle, initiator"></textarea><label data-labels="bind:innerHTML, quickstartdesc"></label><hr/><textarea class="session-desc" name="description" data-model="bind:value, description" data-labels="bind: placeholder, quickstartdescplaceholder"></textarea><div class="newmub-footer"><p class="send"><label class="clear" data-labels="bind:innerHTML, cancellbl" data-newmubevent="listen: touchstart, press; listen:touchend, clear">Clear</label><label class="create" data-labels="bind:innerHTML, sendlbl" data-newmubevent="listen:touchstart, press; listen:touchend, create">Create</label><label class="editerror" data-errormsg="bind:innerHTML, errormsg"></label></p></div></form></div>';
-                
-                widget.place(document.getElementById("newmub"));
-                
-                widget.reset = function reset(){        
+                                "chat" : []};
+                        session.reset(sessionTemplate);       
                 };
                 
                 widget.press = function(event, node){

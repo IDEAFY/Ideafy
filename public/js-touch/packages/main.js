6f0d3fc053605812223b83063f68e0ef5a5fe8cf
Vincent Weyl <vincent.weyl@taiaut.com>
2013-04-01 08:22:49 +0200

    .

--- public/js-touch/packages/main.js
+++ public/js-touch/packages/main.js
@@ -1,4 +1,4 @@
 /* socket io outsode */
-require(["lib/socket.io.min", "lib/couchdbtools","lib/emily",  "lib/olives", "lib/amy2"], function(){
+require(["lib/socket.io", "lib/couchdbtools","lib/emily",  "lib/olives", "lib/amy2"], function(){
 	require(["body"]);
 });

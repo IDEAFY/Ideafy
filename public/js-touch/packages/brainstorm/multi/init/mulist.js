79a2941bb071f88f52292c9034e913236bcb16ac
Vincent Weyl <vincent.weyl@taiaut.com>
2013-04-02 10:29:30 +0200

    .

--- public/js-touch/packages/brainstorm/multi/init/mulist.js
+++ public/js-touch/packages/brainstorm/multi/init/mulist.js
@@ -12,9 +12,13 @@ define(["OObject", "Bind.plugin", "Event.plugin", "CouchDBStore", "service/confi
            
                 var widget = new Widget(),
                     muCDB = new CouchDBStore(),
+                    muSearch = new CouchDBstore(),
                     musessions = new Store([]),
+                    muListOptions = new Store(),
+                    currentList = "mulistall",
                     labels=Config.get("labels");
                 
+                muSearch.setTransport(Config.get("transport"));
                 muCDB.setTransport(Config.get("transport"));
                 
                 widget.plugins.addAll({
@@ -23,19 +27,64 @@ define(["OObject", "Bind.plugin", "Event.plugin", "CouchDBStore", "service/confi
                                 setParticipants : function (array){
                                         this.innerHTML = array.length + 1;
                                 }
-                        })
+                        }),
+                        "musearch" : new Model(muSearch, {
+                                setParticipants : function (array){
+                                        this.innerHTML = array.length + 1;
+                                }        
+                        }),
+                        "mulistevent" : new Event(widget)
                 });
                 
-                widget.template = '<div id="mulist"><div id="mulist-content"><legend data-labels="bind:innerHTML, selectsession"></legend><div class="mulistoptions"><input class="search" type="search"><div class="mumode">Mode</div><div class="mulang">Lang</div></div><hr/><div class="mulistheader"></div><ul id="mulistall" data-muall="foreach"><li><div data-muall="bind:innerHTML, value.mode">Type</div><div data-muall="bind:innerHTML, value.title">Title</div><div data-muall="bind:innerHTML, value.lang">Lang</div><div data-muall="bind:innerHTML, value.initiator.username">Leader</div><div data-muall="bind: setParticipants, value.participants"></div></li></ul><ul id="mucamp invisible"></ul><ul id="musearch invisible"></ul></div></div>';
+                widget.template = '<div id="mulist"><div id="mulist-content"><legend data-labels="bind:innerHTML, selectsession"></legend><div class="mulistoptions"><input class="search" type="search" data-mulistevent="listen: keypress, search"><div class="mumode">Mode</div><div class="mulang">Lang</div></div><hr/><div class="mulistheader"></div><ul id="mulistall" data-muall="foreach"><li><div data-muall="bind:innerHTML, value.mode">Type</div><div data-muall="bind:innerHTML, value.title">Title</div><div data-muall="bind:innerHTML, value.lang">Lang</div><div data-muall="bind:innerHTML, value.initiator.username">Leader</div><div data-muall="bind: setParticipants, value.participants"></div></li></ul><ul id="musort invisible"></ul><ul id="musearch invisible"><li><div data-musearch="bind:innerHTML, value.mode">Type</div><div data-musearch="bind:innerHTML, value.title">Title</div><div data-musearch="bind:innerHTML, value.lang">Lang</div><div data-musearch="bind:innerHTML, value.initiator.username">Leader</div><div data-musearch="bind: setParticipants, value.participants"></div></li></ul></div></div>';
                 
                 widget.place(document.getElementById("mulist"));
                 
                 widget.reset = function reset(){
-                        
+                       currentList = "mulistall"; 
                         // synchronize muCDB with database
-                        muCDB.sync(Config.get("db"), "library", "_view/waitingsessions", {descending:true});
+                        muCDB.syncAll();
+                };
+                
+                widget.syncAll = function syncAll(){
+                        var promise = new Promise();
+                        muCDB.unsync();
+                        muCDB.reset([]);
+                        muCDB.sync(Config.get("db"), "library", "_view/waitingsessions", {descending:true}).then(function(){
+                                promise.fulfill();
+                        });
+                        return promise;
+                };
+                
+                widget.syncSearch = function syncSearch(query){
+                        var promise = new Promise();
+                        muSearch.unsync();
+                        muSearch.reset([]);
+                        muSearch.sync(Config.get("db"), "_fti/local/indexedsessions", "_view/roulette", {descending:true}).then(function(){
+                                console.log(muSearch.toJSON());
+                                promise.fulfill();
+                        });
                         
-                        
+                        return promise;        
+                };
+                
+                widget.search = function(event, node){
+                        if (event.keyCode === 13){
+                                if (node.value === ""){
+                                        widget.toggleList("mulistall");
+                                }
+                                else {
+                                        widget.toggleList("musearch");
+                                        widget.syncSearch();
+                                }
+                        }        
+                };
+                
+                widget.toggleList = function toggleList(list){
+                        if (list !== currentList){
+                                document.getElementById(currentList).classList.add("invisible");
+                                document.getElementById(list).classList.remove("invisible");
+                        }
                 };
                 
                 return widget;

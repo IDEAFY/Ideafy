e973f0b6b843c34279208cd41a35ad5a55579af6
Vincent Weyl <vincent.weyl@taiaut.com>
2013-04-04 13:10:48 +0200

    .

--- public/js-touch/packages/login.js
+++ public/js-touch/packages/login.js
@@ -6,7 +6,7 @@
  */
 
 define(["OObject" ,"Amy/Stack-plugin", 
-	"service/map", "Amy/Delegate-plugin", "service/config", "Bind.plugin", "lib/spin"],
+	"service/map", "Amy/Delegate-plugin", "service/config", "Bind.plugin", "lib/spin.min"],
 	function(Widget, Stack, Map, Event, Config, Model, Spinner){
 		return function LoginConstructor(){
 		//declaration

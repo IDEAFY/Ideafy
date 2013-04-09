9aa30888c043a1f31ce830bc20282f7ef3e106c2
Vincent Weyl <vincent.weyl@taiaut.com>
2013-04-09 15:30:03 +0200

    .

--- public/js-touch/packages/brainstorm/multi/init/mulist.js
+++ public/js-touch/packages/brainstorm/multi/init/mulist.js
@@ -14,7 +14,7 @@ define(["OObject", "Bind.plugin", "Event.plugin", "CouchDBStore", "service/confi
                     muListAll = new Store([]),
                     muSearch = new Store([]),
                     musessions = new Store([]),
-                    muListOptions = new Store(),
+                    muListOptions = new Store({"lang":[]}),
                     currentList = "mulistall",
                     labels=Config.get("labels"),
                     user = Config.get("user"),
@@ -24,6 +24,15 @@ define(["OObject", "Bind.plugin", "Event.plugin", "CouchDBStore", "service/confi
                 
                 widget.plugins.addAll({
                         "labels" : new Model(labels),
+                        "options" : new Model(muListOptions,{
+                                setLang : function(lang){
+                                        var i,l, res="";
+                                        for (i=0, l=lang.length;i<l;i++){
+                                                res+='<option "class = '+lang[i]+'"</option>';
+                                        }
+                                        this.innerHTML=res;        
+                                   }        
+                        }),
                         "muall" : new Model(muListAll, {
                                 setParticipants : function (array){
                                         var length = array.length + 1, i, res = '<ul>';
@@ -96,7 +105,7 @@ define(["OObject", "Bind.plugin", "Event.plugin", "CouchDBStore", "service/confi
                         "mulistevent" : new Event(widget)
                 });
                 
-                widget.template = '<div id="mulist"><div id="mulist-content"><div id="mulistspinner"></div><legend data-labels="bind:innerHTML, selectsession"></legend><div class="mulistoptions"><input class="search" type="search" data-mulistevent="listen: keypress, search"><div class="mumode">Mode</div><div class="mulang">Lang</div></div><hr/><div class="mulistheader"><div class="mumode">Mode</div><div class="mutitle">Title</div><div class="mulang">Lang</div><div class="muleadername">Session leader</div><div class="muparts">Participants</div></div><div id="noresult" class="invisible">no session found</div><ul id="mulistall" data-muall="foreach"><li data-mulistevent="listen: touchstart, zoom"><div class="mumode" data-muall="bind:setMode, value.mode"></div><div class="mutitle" data-muall="bind:innerHTML, value.title">Title</div><div class="mulang" data-muall="bind:setLang, value.lang"></div><div class="muleadername" data-muall="bind:innerHTML, value.initiator.username">Leader</div><div class="muparts" data-muall="bind: setParticipants, value.participants"></div></li></ul><ul id="musort" class="invisible"></ul><ul id="musearch" class="invisible" data-musearch="foreach"><li data-mulistevent="listen: touchstart, zoom"><div class="mumode" data-musearch="bind:setMode, fields.mode"></div><div class="mutitle" data-musearch="bind:innerHTML, fields.title">Title</div><div class="mulang" data-musearch="bind:setLang, fields.lang"></div><div class="muleadername" data-musearch="bind:innerHTML, fields.initiator">Leader</div><div class="muparts" data-musearch="bind: setParticipants, fields.participants"></div></li></ul></div></div>';
+                widget.template = '<div id="mulist"><div id="mulist-content"><div id="mulistspinner"></div><legend data-labels="bind:innerHTML, selectsession"></legend><div class="mulistoptions"><input class="search" type="search" data-mulistevent="listen: keypress, search"><select class="mumode"><option>All</option><option>Roulette</option><option>Campfire</option><option>Boardroom</option></select><select class="mulang" data-options="bind: setLang, lang"></select></div><hr/><div class="mulistheader"><div class="mumode">Mode</div><div class="mutitle">Title</div><div class="mulang">Lang</div><div class="muleadername">Session leader</div><div class="muparts">Participants</div></div><div id="noresult" class="invisible" data-labels="bind:innerHTML, nosessionfound"></div><ul id="mulistall" data-muall="foreach"><li data-mulistevent="listen: touchstart, zoom"><div class="mumode" data-muall="bind:setMode, value.mode"></div><div class="mutitle" data-muall="bind:innerHTML, value.title">Title</div><div class="mulang" data-muall="bind:setLang, value.lang"></div><div class="muleadername" data-muall="bind:innerHTML, value.initiator.username">Leader</div><div class="muparts" data-muall="bind: setParticipants, value.participants"></div></li></ul><ul id="musort" class="invisible"></ul><ul id="musearch" class="invisible" data-musearch="foreach"><li data-mulistevent="listen: touchstart, zoom"><div class="mumode" data-musearch="bind:setMode, fields.mode"></div><div class="mutitle" data-musearch="bind:innerHTML, fields.title">Title</div><div class="mulang" data-musearch="bind:setLang, fields.lang"></div><div class="muleadername" data-musearch="bind:innerHTML, fields.initiator">Leader</div><div class="muparts" data-musearch="bind: setParticipants, fields.participants"></div></li></ul></div></div>';
                 
                 widget.reset = function reset(){
                        currentList = "mulistall";
@@ -193,6 +202,12 @@ define(["OObject", "Bind.plugin", "Event.plugin", "CouchDBStore", "service/confi
                         }
                 };
                 
+                
+                // init
+                // get available languages
+                   transport.request("GetLanguages", {}, function(result){
+                        muListOptions.set("lang", ["all"].concat(result));      
+                   });
                 MUALL = muListAll;
                 return widget;
                    

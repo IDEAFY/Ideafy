d3af09916d091ba8e08866efa5ae6c4f9329c4e6
Vincent Weyl <vincent.weyl@taiaut.com>
2013-04-11 10:16:36 +0200

    .

--- public/js-touch/packages/main.js
+++ public/js-touch/packages/main.js
@@ -1,7 +1,6 @@
 /* socket io outsode */
 require(["lib/socket.io", "lib/couchdbtools","lib/emily",  "lib/olives", "lib/amy2", "lib/spin.min"], function(){
 	require(["body"], function(Body){
-	        var body = new Body();
-	        console.log(body);
+	        Body();
 	});
 });

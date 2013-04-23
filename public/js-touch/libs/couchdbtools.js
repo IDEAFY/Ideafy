0d09b8f6c3c3fc8a002070dc16c68da9e90e357d
Vincent Weyl <vincent.weyl@taiaut.com>
2013-04-23 07:24:36 +0200

    .

--- public/js-touch/libs/couchdbtools.js
+++ public/js-touch/libs/couchdbtools.js
@@ -320,7 +320,6 @@ function CouchDBStore(Store, StateMachine, Tools, Promise) {
                                         },
                                 function (changes) {
                                         var json;
-                                        console.log(changes);
                                         // Should I test for this very special case (heartbeat?)
                                         // Or do I have to try catch for any invalid json?
                                         if (changes == "\n") {
@@ -361,7 +360,6 @@ function CouchDBStore(Store, StateMachine, Tools, Promise) {
                                         },
                                         function (changes) {
                                                 var json;
-                                                console.log(changes);
                                                 // Should I test for this very special case (heartbeat?)
                                                 // Or do I have to try catch for any invalid json?
                                                 if (changes == "\n") {

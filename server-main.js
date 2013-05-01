f133895d41cc1a53213515607110f90d30b19a5f
Vincent Weyl <vincent.weyl@taiaut.com>
2013-05-01 13:01:34 +0200

    .

--- server-main.js
+++ server-main.js
@@ -1449,7 +1449,7 @@ CouchDBTools.requirejs(["CouchDBUser", "Transport", "CouchDBDocument", "CouchDBV
 
         // updating a session's score
         olives.handlers.set("UpdateSessionScore", function(json, onEnd){
-                var cdb = new CouchDBDocument(), increment, min_score, bonus, coeff, wbdata, t, input,
+                var cdb = new CouchDBDocument(), increment = 0, min_score, bonus, coeff, wbdata, t, input,
                     updateUserWithSessionScore = function(sessionCDB){
                         var promise = new Promise(),
                             ip = sessionCDB.get("score"),
@@ -1501,7 +1501,7 @@ CouchDBTools.requirejs(["CouchDBUser", "Transport", "CouchDBDocument", "CouchDBV
                                 if (t<600000) {coeff = 1.5;} // great !!
                                 if (t<300000) {coeff = 0.75;} // too fast
                                 if (t<120000) {coeff = 0.25;} // way too fast
-                                if (input.title.length+input.story.length+input.solution.length < 200) {coeff *= 0.5;} // need a bit more effort
+                                if (input.title.length+input.story.length+input.solution.length < 300) {coeff *= 0.5;} // need a bit more effort
                                 if (wbdata.length>12) {coeff *= 1.25;}
                                 else {
                                         if (wbdata.length < 3) {coeff *= 0.25;}
@@ -1548,7 +1548,9 @@ CouchDBTools.requirejs(["CouchDBUser", "Transport", "CouchDBDocument", "CouchDBV
                 if (increment !== 0 || json.step.search("idea")>-1){
                         getDocAsAdmin(json.sid, cdb)
                         .then(function(){
-                                cdb.set("score", parseInt(cdb.get("score")+increment, 10));
+                                var currentScore = cdb.get("score"),
+                                    newScore = parseInt(currentScore, 10) + increment;
+                                cdb.set("score", newScore);
                                 return updateDocAsAdmin(json.sid, cdb);
                         })
                         .then(function(){

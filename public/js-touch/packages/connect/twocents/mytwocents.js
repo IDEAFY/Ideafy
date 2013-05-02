f8bcc44c02893b8b5de61930a0af2b5c20a4ffe8
Vincent Weyl <vincent.weyl@taiaut.com>
2013-05-02 15:39:43 +0200

    .

--- public/js-touch/packages/connect/twocents/mytwocents.js
+++ public/js-touch/packages/connect/twocents/mytwocents.js
@@ -245,9 +245,11 @@ define(["OObject", "service/map", "service/config", "Bind.plugin", "Place.plugin
                                 contactList.reset([]);
                                 connections = user.get("connections");
                                 for (i=0, l=connections.length; i<l; i++){
-                                        if (connections[i].type === "user") contactList.alter("push", {"contact":connections[i], "selected":false})
+                                        if (connections[i].type === "user") {
+                                                contactList.alter("push", {"contact":connections[i], "selected":false});
+                                        }
                                 }
-                                myTwoq.resetQuery({key: Config.get("uid"), descending: true})
+                                mytwoq.resetQuery({key: Config.get("uid"), descending: true})
                                 .then(function(){
                                         mtcStack.getStack().show("#mytwoq");
                                         mtcDetails.reset("default");

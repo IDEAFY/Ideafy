d36459ab31431ffaf09949f939a45e7721400dbd
Vincent Weyl <vincent.weyl@taiaut.com>
2013-05-03 05:45:41 +0200

    .

--- public/js-touch/packages/dashboard/profile/leaderboard.js
+++ public/js-touch/packages/dashboard/profile/leaderboard.js
@@ -13,14 +13,14 @@ define(["OObject", "service/config", "Bind.plugin", "Event.plugin", "CouchDBView
                         var leaderboard = new Widget(),
                             leaders = new CouchDBView([]);
                         
-                        leaderboard.template = '<div><ul data-leaders="foreach"><li class="leader" data-leaders="bind: setSpotLight, value.userid"><div data-leaders="bind:setAvatar, value.userid"></div><div class="username" data-leaders="bind:innerHTML, value.username"></div><div class="distinction" data-leaders="bind:setDistinction, value.ip"></div><div class="grade" data-leaders="bind:setGrade, value.ip"></div><div class="score" data-leaders="bind: setScore, value.ip"></div></li></ul></div>';
+                        leaderboard.template = '<div><ul data-leaders="foreach"><li class="leader" data-leaders="bind:setSpotLight, value.userid"><div data-leaders="bind:setAvatar, value.userid"></div><div class="username" data-leaders="bind:innerHTML, value.username"></div><div class="distinction" data-leaders="bind:setDistinction, value.ip"></div><div class="grade" data-leaders="bind:setGrade, value.ip"></div><div class="score" data-leaders="bind: setScore, value.ip"></div></li></ul></div>';
                         
                         leaderboard.plugins.addAll({
                                 "leaders": new Model(leaders,{
                                         setSpotLight : function(userid){
                                                 if (userid === Config.get("user").get("_id")){
                                                         this.classList.add("userleader");
-                                                        this.scrollIntoView();
+                                                        this.scrollIntoView(false);
                                                 }
                                                 else{
                                                         this.classList.remove("userleader");
@@ -28,10 +28,12 @@ define(["OObject", "service/config", "Bind.plugin", "Event.plugin", "CouchDBView
                                         },
                                         setAvatar : function(userid){
                                                 var _ui, _frag;
-                                                _frag = document.createDocumentFragment();
-                                                _ui = new Avatar([userid]);
-                                                _ui.place(_frag);
-                                                (!this.hasChildNodes())?this.appendChild(_frag):this.replaceChild(_frag, this.firstChild);
+                                                if (userid){
+                                                        _frag = document.createDocumentFragment();
+                                                        _ui = new Avatar([userid]);
+                                                        _ui.place(_frag);
+                                                        (!this.hasChildNodes())?this.appendChild(_frag):this.replaceChild(_frag, this.firstChild);
+                                                }
                                         },
                                         setGrade : function(ip){
                                                 var node = this;

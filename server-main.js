15bdbe2dbe04ec829f77486ebcf676d0e1f8d2b7
Vincent Weyl <vincent.weyl@taiaut.com>
2013-05-13 07:19:59 +0200

    .

--- server-main.js
+++ server-main.js
@@ -137,11 +137,9 @@ CouchDBTools.requirejs(["CouchDBUser", "Transport", "CouchDBDocument", "CouchDBV
        io.enable('browser client minification');  // send minified client
        io.enable('browser client etag');          // apply etag caching logic based on version number
        io.enable('browser client gzip');          // gzip the file
-       io.set('log level', 3);                    // reduce logging
-       io.set('transports', [                     // enable all transports (optional if you want flashsocket)
-                        'websocket', 'flashsocket', 'htmlfile', 'xhr-polling', 'jsonp-polling']);
-        io.set("close timeout", 15);
-        io.set("heartbeat interval", 10);
+       io.set('log level', 0);                    // reduce logging
+        io.set("close timeout", 60);
+        io.set("heartbeat interval", 25);
         
         // we need lots of sockets
         http.globalAgent.maxSockets = Infinity;

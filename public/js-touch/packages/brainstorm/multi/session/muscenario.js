43f2243c4e8f4c8ceb794432e68133342b6b382b
Vincent Weyl <vincent.weyl@taiaut.com>
2013-05-13 15:49:52 +0200

    .

--- public/js-touch/packages/brainstorm/multi/session/muscenario.js
+++ public/js-touch/packages/brainstorm/multi/session/muscenario.js
@@ -561,11 +561,12 @@ define(["OObject", "service/map", "Bind.plugin", "Event.plugin", "Place.plugin",
                                         
                                         // avoid upload if $session is already up-to-date (e.g. replay)
                                         if ($session.get("scenarioWB") !== _wbContent.getNbItems() || JSON.stringify($session.get("scenarioWB")) !== _wbContent.toJSON()){
-                                                $session.unsync();
                                                 $session.set("scenarioWB", JSON.parse(_wbContent.toJSON()));
                                                 $session.upload()
                                                 .then(function(response){
-                                                       console.log(response);
+                                                       console.log("success : ", response);
+                                                }, function(response){
+                                                       console.log("failure : ", response);
                                                 });
                                         }
                                         else{

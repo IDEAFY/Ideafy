a2f8ea478ce223151b6b4e924d35f57bc0aff731
Vincent Weyl <vincent.weyl@taiaut.com>
2013-05-21 09:35:29 +0200

    .

--- public/js-touch/services/avatar.js
+++ public/js-touch/services/avatar.js
@@ -47,7 +47,7 @@ define(["OObject", "Bind.plugin", "Event.plugin", "service/config", "Store", "se
                         else if (_avatars.get(_id)){
                                 if (_avatars.get(_id) !== "in progress"){
                                         _avatars.watchValue(_id, function(val){
-                                                (val && (val !== "in progress")){
+                                                if (val && (val !== "in progress")){
                                                         _store.set("img", val);
                                                 }
                                         });

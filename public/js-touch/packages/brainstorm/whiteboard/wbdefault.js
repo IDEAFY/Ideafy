768288273b28e3f89fba9fde398810ee36ecc860
Vincent Weyl <vincent.weyl@taiaut.com>
2013-06-03 09:58:52 +0200

    .

--- public/js-touch/packages/brainstorm/whiteboard/wbdefault.js
+++ public/js-touch/packages/brainstorm/whiteboard/wbdefault.js
@@ -12,12 +12,16 @@ define(["OObject", "service/config", "Bind.plugin"],
              
                 var _widget = new Widget(),
                     _helplbl = "",
-                    _labels = Config.get("labels");
+                    _labels = Config.get("labels"),
+                    _sessionType = "quick"; // current session type ("quick" or "mu")
                     
                 _widget.plugins.add("labels", new Model(_labels));
                 
+                //get session type
+                (document.getElementById("musession")) ? _sessionType = "mu" : _sessionType = "quick";
+                
                 //if whiteboard is of type scenario display appropriate help message
-                ($type === "scenario") ? _helplbl = "quickscenariohelp" : _helplbl = "quickideahelp";
+                ($type === "scenario") ? _helplbl = _sessionType +"scenariohelp" : _helplbl = _sessionType + "ideahelp";
                 
                 _widget.template = '<div id="whiteboard-default" class="defaultcontent"><div class="doctor-deedee"></div><div class="help" data-labels="bind:innerHTML,'+_helplbl+'"></div></div>';
                 

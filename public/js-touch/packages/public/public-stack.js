3dd64fedc93c7662ae4b9dce336567d14690dc7e
Vincent Weyl <vincent.weyl@taiaut.com>
2013-06-07 05:40:52 +0200

    .

--- public/js-touch/packages/public/public-stack.js
+++ public/js-touch/packages/public/public-stack.js
@@ -54,7 +54,7 @@ define(["OObject", "service/map", "Amy/Stack-plugin", "./detail-stack/public-ide
                                              break;
                                              
                                         case "#public-share":
-                                                _stack.getStack().get("#public-share").reset(_store.get(current).doc);
+                                                _stack.getStack().get("#public-share").reset(_store.get(current).id);
                                                 _stack.getStack().show("#public-share");
                                              break;
                                         case "close":
@@ -77,7 +77,7 @@ define(["OObject", "service/map", "Amy/Stack-plugin", "./detail-stack/public-ide
 			};
 			
 			_widget.share = function share(idea){
-			_stack.getStack().get("#public-share").reset(idea);
+			_stack.getStack().get("#public-share").reset(idea._id);
                                  _stack.getStack().show("#public-share");         
 			};
 			

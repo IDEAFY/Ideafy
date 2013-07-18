3962fd40381c8c3cd91f9deba39de1a057d84981
Vincent Weyl <vincent.weyl@taiaut.com>
2013-07-18 12:39:17 +0200

    .

--- public/js-touchmin/build.js
+++ public/js-touchmin/build.js
@@ -8,7 +8,7 @@
 /* deps load before main script??*/
 require.config({
         waitSeconds: 45,
-	baseUrl : "js-min/packages",
+	baseUrl : "js-touchmin/packages",
         paths: {
                 lib : "../libs",
                 service : "../services",

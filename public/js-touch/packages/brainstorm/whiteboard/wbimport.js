98cb9afc300075f68935f85fa11af331eb7f529c
Vincent Weyl <vincent.weyl@taiaut.com>
2013-07-19 11:40:10 +0200

    .

--- public/js-touch/packages/brainstorm/whiteboard/wbimport.js
+++ public/js-touch/packages/brainstorm/whiteboard/wbimport.js
@@ -86,7 +86,7 @@ define(["OObject", "service/map", "service/config", "Bind.plugin", "Event.plugin
                                         var json, node=this, _transport = Config.get("transport");
                                         if (!content) this.innerHTML = ""
                                         else {
-                                                node.parentNode.scrollIntoView(); 
+                                                document.querySelector(".picframe").scrollIntoView(); 
                                                 json = {"dir":"sessions/"+_sid, "filename":content};
                                                 _transport.request("GetFile", json, function(data){
                                                         var _img = new Image(),
@@ -94,7 +94,7 @@ define(["OObject", "service/map", "service/config", "Bind.plugin", "Event.plugin
                                                         _img.src = data;
                                                         node.width=_img.width;
                                                         node.height=_img.height;
-                                                        _ctx.drawImage(_img,0,0); 
+                                                        _ctx.drawImage(_img,0,0);
                                                 });
                                         }       
                                 }

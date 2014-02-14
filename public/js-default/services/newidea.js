b9f8f627c95fd11c810ac180d50c487d5eb2cb79
Vincent Weyl <vincent.weyl@taiaut.com>
2014-02-13 16:07:31 -0800

    .

--- public/js-default/services/newidea.js
+++ public/js-default/services/newidea.js
@@ -130,7 +130,7 @@ define(["OObject", "service/map", "Bind.plugin", "Event.plugin", "service/config
                                         setBg : function(cat){
                                                 var colors = ["9ac9cd", "#f27b3d", "#bd262c", "#5f8f28", "#657b99", "#a000a1"], node =this;
                                                 
-                                                node.setAttribute("style": "background-color: transparent;");
+                                                node.setAttribute("style", "background-color: transparent;");
                                                 
                                                 Config.get("cat").forEach(function(val, idx){
                                                         if (_labels.get(val) === cat) node.setAttribute("style","background-color:"+ colors[idx]);

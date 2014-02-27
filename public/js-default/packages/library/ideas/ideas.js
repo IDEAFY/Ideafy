2aa51698cdae5cba313a7e06dd6f6955eaa57ac2
Vincent Weyl <vincent.weyl@taiaut.com>
2014-02-27 09:15:15 -0800

    .

--- public/js-default/packages/library/ideas/ideas.js
+++ public/js-default/packages/library/ideas/ideas.js
@@ -104,8 +104,8 @@ define(["OObject", "Amy/Control-plugin" ,
 				
 				// if an idea has been added by author make sure it is displayed
 				_ideaList.watch("added", function(v, i){
-				            console.log(val, i);
-                                            _detail.reset(_ideaList, val);            
+				            console.log(v, i);
+                                            _detail.reset(_ideaList, v);            
                                 });
 				
 			};

cf1b7a14d6ed5a11ab3d50051315698565cec816
Vincent Weyl <vincent.weyl@taiaut.com>
2014-03-02 09:37:00 -0800

    .

--- public/js-default/packages/twocents/writetwocentreply.js
+++ public/js-default/packages/twocents/writetwocentreply.js
@@ -76,9 +76,9 @@ define(["OObject", "Store", "Bind.plugin", "Event.plugin", "service/config", "se
                                         
                                         json = {docId: currentIdea, type: type, position: position, twocent: currentTwocent, reply: content};
                                         transport.request("WriteTwocent", json, function(result){
+                                                console.log(result);
                                                 node.setAttribute("style", "background: #8cab68;");
                                                 publishSpinner.stop(); 
-                                                console.log(result);
                                                 if (result !== "ok"){
                                                         alert(Config.get("labels").get("somethingwrong"));        
                                                 }

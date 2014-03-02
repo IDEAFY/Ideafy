3dcda045daedf464cbaedf8f8c916d11d6c6e0f4
Vincent Weyl <vincent.weyl@taiaut.com>
2014-03-02 07:34:41 -0800

    .

--- rbuild.js
+++ rbuild.js
@@ -1,6 +1,6 @@
 ({		    
-                    baseUrl: "public/js-ios/packages",
-                    mainConfigFile: "public/js-ios/build.js",
+                    baseUrl: "public/js-default/packages",
+                    mainConfigFile: "public/js-default/build.js",
                     dir: "built",
 		    name: "main",
                     optimize: "uglify2",

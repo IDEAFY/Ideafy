907ec4e022c6cf3ffb7484b37fbe9d4a04a34e93
Vincent Weyl <vincent.weyl@taiaut.com>
2014-03-19 16:31:03 -0700

    debugging session timing

--- public/js-default/packages/brainstorm/multi/init/newmub.js
+++ public/js-default/packages/brainstorm/multi/init/newmub.js
@@ -433,6 +433,7 @@ define(["OObject", "Bind.plugin", "Event.plugin", "CouchDBDocument", "service/co
                         
                         // check if session is scheduled (ie start date > now+10min)
                         scheduled = dateUI.getDatestamp() + timeUI.getTimestamp();
+                        console.log(scheduled, now.getTime(), dateUI.getDate(), timeUI.getTime());
                         if ((scheduled - now.getTime()) > 600000) session.set("scheduled", scheduled);
                         session.set("date", dateUI.getDate());
                         

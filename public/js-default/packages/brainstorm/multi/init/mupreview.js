abec4c38baa0504388bc4e7083ac91ab30d7bac6
Vincent Weyl <vincent.weyl@taiaut.com>
2014-03-26 15:12:57 -0700

    using a regexp to get rid of seconds in time string in mupreview

--- public/js-default/packages/brainstorm/multi/init/mupreview.js
+++ public/js-default/packages/brainstorm/multi/init/mupreview.js
@@ -59,7 +59,7 @@ define(["OObject", "service/config", "CouchDBDocument", "Store", "Bind.plugin", 
                                                         time = new Date(scheduled);
                                                         now = new Date();
                                                         if ((time.getTime() - now.getTime()) <= 300000) this.innerHTML = labels.get("now");
-                                                        else this.innerHTML = time.toLocaleTimeString();
+                                                        else this.innerHTML = time.toLocaleTimeString().replace(/:\d\d /, '');
                                                 }
                                         },
                                         setAvatar : function setAvatar(id){

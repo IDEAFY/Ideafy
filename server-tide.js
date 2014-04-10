81b698288c45505b4182c58c9465c1fba02d9260
Vincent Weyl <vincent.weyl@taiaut.com>
2014-04-10 13:41:31 -0700

    .

--- server-tide.js
+++ server-tide.js
@@ -94,8 +94,15 @@ CouchDBTools.requirejs(["CouchDBUser", "Transport", "CouchDBDocument", "CouchDBV
                 .use('/upload', srvUtils.uploadFunc)
                 .use('/downloads', srvUtils.downloadFunc)   
                 .use(function(req, res, next) {
+                        var ori = req.headers.origin || "http://tide.ideafy.com";
+                        res.setHeader("Access-Control-Allow-Origin", ori);
+                        res.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
+                        res.setHeader("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE,OPTIONS');
                         res.setHeader("Ideady Server", "node.js/" + process.versions.node);
                         res.setHeader("X-Powered-By", "OlivesJS + Connect + Socket.io");
+                        if ('OPTIONS' == req.method) {
+                                res.send(200);
+                        }
                         next();
                 })
                 .use(connect.cookieParser())

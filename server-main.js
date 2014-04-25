1fe5432b29896dea343ff00f840bbe1cfdb57c81
Vincent Weyl <vincent.weyl@taiaut.com>
2014-04-25 16:02:06 -0700

    .

--- server-main.js
+++ server-main.js
@@ -23,7 +23,7 @@ var http = require("http"),
 //    path = require("path"), 
 //     qs = require("querystring"), 
  //   url = require("url"), 
-//    st = require("st"),
+    st = require("st"),
 //    redirect = require('connect-redirection'), 
     sessionStore = new RedisStore({
         hostname : "127.0.0.1",
@@ -82,7 +82,7 @@ badges;
 
 
 // Mount the static directory to be cached
-// var mount = st({path: __dirname + '/public', url: '/public'});
+var mount = st({path: __dirname + '/public', url: '/public'});
 
 /*****************************
  *  APPLICATION SERVER

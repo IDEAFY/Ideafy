abb4abc5b9b60af975cc1a2cd90e0b1823a9bcd5
Vincent Weyl <vincent.weyl@taiaut.com>
2014-05-04 11:24:16 -0700

    .

--- public/js-default/packages/brainstorm/multi/init/mulist.js
+++ public/js-default/packages/brainstorm/multi/init/mulist.js
@@ -87,7 +87,6 @@ define(["OObject", "Bind.plugin", "Event.plugin", "CouchDBView", "service/config
                                         }
                                 },
                                 setDate : function(scheduled){
-                                        console.log(scheduled);
                                         var now, sched;
                                         if (scheduled){
                                                 now = new Date();

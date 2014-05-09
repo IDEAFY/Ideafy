8b3aa915776c49322b4dc256dc5ba2df6f366e24
Vincent Weyl <vincent.weyl@taiaut.com>
2014-05-08 19:22:57 -0700

    removing timeout when leader deletes an immediate session if no participants has joined yet

--- public/js-default/packages/brainstorm/multi/mubwait.js
+++ public/js-default/packages/brainstorm/multi/mubwait.js
@@ -205,7 +205,11 @@ define(["OObject", "Store", "CouchDBDocument", "service/map", "Bind.plugin", "Ev
                         // initiator decides to cancel the session
                        widget.cancelSession = function cancelSession(){
                                 var countdown = 5000;
-                                if (!session.get("participants").length) {countdown = 2000;}
+                                if (!session.get("participants").length) {
+                                        confirmUI.hide();
+                                        $exit;
+                                        session.remove();
+                                }
                                 widget.displayInfo("deleting", countdown).then(function(){
                                         session.remove();
                                         $exit();

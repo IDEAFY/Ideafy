a7e24bca8a1c23005c63ce82c12c611410ad6627
Vincent Weyl <vincent.weyl@taiaut.com>
2014-05-12 11:54:28 -0700

    instantiated Help popup

--- public/js-default/packages/dock.js
+++ public/js-default/packages/dock.js
@@ -13,7 +13,7 @@ define(["OObject", "Place.plugin", "Amy/Stack-plugin", "Amy/Control-plugin",
 
 		//declaration
 			var _widget = new Widget(),
-			    _newIdea, _new2q, _new2c, _tips, _notify, _confirm, _help,
+			    _new2q, _new2c, _tips, _notify, _confirm,
 			    _public, _library, _brainstorm, _connect, _dashboard,
 			    _control = new Control(this),
 			    _observer = Config.get("observer"),
@@ -22,8 +22,6 @@ define(["OObject", "Place.plugin", "Amy/Stack-plugin", "Amy/Control-plugin",
                 
                 
                 // initialize popups
-                _help = new Help();
-                console.log("help ok");
                 _notify= new Notify();
                 console.log("notify ok");
                 _new2q = new New2Q();
@@ -40,7 +38,7 @@ define(["OObject", "Place.plugin", "Amy/Stack-plugin", "Amy/Control-plugin",
 			_widget.plugins.addAll({
 				"dockstack" : _stack,
 				"dockcontrol" : _control,
-				"place" : new Place({"notify":_notify, "newidea": NewIdea, "new2q": _new2q, "new2c": _new2c, "help": _help, "tips": _tips, "confirm": _confirm})
+				"place" : new Place({"notify":_notify, "newidea": NewIdea, "new2q": _new2q, "new2c": _new2c, "help": Help, "tips": _tips, "confirm": _confirm})
 			});
 			
 			_widget.template = '<div id="wrapper"><nav id="dock" data-dockcontrol="radio:a,selected,mousedown,setCurrentWidget"><a class="dock-item selected" href="#public" data-dockcontrol="init"></a><a class="dock-item" href="#library"></a><a class="dock-item" href="#brainstorm"></a><a class="dock-item" href="#connect"></a><a class="dock-item" href="#dashboard"></a></nav><div class="stack" data-dockstack="destination"></div><div data-place="place:notify"></div><div data-place="place:newidea"></div><div data-place="place:new2q"></div><div data-place="place:new2c"></div><div data-place="place:help"></div><div data-place="place:tips"></div><div data-place="place:confirm"></div></div>';
--- public/js-default/services/help.js
+++ public/js-default/services/help.js
@@ -8,7 +8,7 @@
 define(["OObject", "service/map", "Bind.plugin", "Event.plugin", "service/config", "Store"],
         function(Widget, Map, Model, Event, Config, Store){
                 
-                return function HelpConstructor(){
+                return new function HelpConstructor(){
                 
                         var _widget = new Widget(),
                             _labels = Config.get("labels"),

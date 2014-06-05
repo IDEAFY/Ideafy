3ec5bc8616b70d2cfc577efe6471ad2ef83955d1
Vincent Weyl <vincent.weyl@taiaut.com>
2014-06-05 15:16:25 -0700

    updated services/avatarlist.js for new framework

--- public/js-default/services/avatarlist.js
+++ public/js-default/services/avatarlist.js
@@ -5,47 +5,53 @@
  * Copyright (c) 2014 IDEAFY LLC
  */
 
-define(["OObject", "Bind.plugin", "Event.plugin", "service/config", "service/utils", "Store"],
-        function(Widget, Model, Event, Config, Utils, Store){
-                
-                function AvatarListConstructor($ids){
+var olives = require("../libs/olives"),
+      emily = require("../libs/emily"),
+      Widget = olives.OObject,
+      Model = olives["Bind.plugin"],
+      Event = olives["Event.plugin"],
+      Store = emily.Store,
+      Config = require("./config"),
+      Utils = require("./utils");
 
-                        var _store = new Store([]),
-                            i,
-                            _avatars = Config.get("avatars"); 
-                        // setup
-                        this.plugins.addAll({
-                                "avatar" : new Model(_store, {
-                                        setAvatar : function(img){
-                                                this.setAttribute("style", "background-image: url('"+img+"');");
-                                        }
-                                }),
-                                "event" : new Event(this)
-                        });
-                        
-                        // set template
-                        this.template='<ul data-avatar="foreach"><li data-avatar="bind: setAvatar, img; bind: name, id"></li></ul>';
-                        // init
-                        for (i=0; i<$ids.length; i++){
-                                if ($ids[i] === Config.get("user").get("_id")){
-                                        _store.alter("push", {id:$ids[i], img: Config.get("avatar")});
-                                }
-                                else if (_avatars.get($ids[i])){
-                                        _store.alter("push", {id:$ids[i], img:_avatars.get($ids[i])});       
-                                }
-                                else {
-                                        Config.get("transport").request("GetAvatar", {id: $ids[i]}, function(result){
-                                                        if (!result.error){
-                                                                _store.alter("push", {id: $ids[i], img: result});
-                                                        }
-                                        });
-                                }
+var AvatarListConstructor = function($ids){
+
+        var _store = new Store([]),
+              i,
+             _avatars = Config.get("avatars"); 
+        
+        // setup
+        this.plugins.addAll({
+                "avatar" : new Model(_store, {
+                        setAvatar : function(img){
+                                this.setAttribute("style", "background-image: url('"+img+"');");
                         }
-                             
-                }
-                
-                return function AvatarListFactory($ids){
-                        AvatarListConstructor.prototype = new Widget();
-                        return new AvatarListConstructor($ids);
-                };      
+                }),
+                "event" : new Event(this)
         });
+                        
+        // set template
+        this.template='<ul data-avatar="foreach"><li data-avatar="bind: setAvatar, img; bind: name, id"></li></ul>';
+                        
+        // init
+        for (i=0; i<$ids.length; i++){
+                if ($ids[i] === Config.get("user").get("_id")){
+                        _store.alter("push", {id:$ids[i], img: Config.get("avatar")});
+                }
+                else if (_avatars.get($ids[i])){
+                        _store.alter("push", {id:$ids[i], img:_avatars.get($ids[i])});       
+                }
+                else {
+                        Config.get("transport").request("GetAvatar", {id: $ids[i]}, function(result){
+                                if (!result.error){
+                                        _store.alter("push", {id: $ids[i], img: result});
+                                }
+                        });
+                }
+        }
+};
+                
+module.exports = function AvatarListFactory($ids){
+        AvatarListConstructor.prototype = new Widget();
+        return new AvatarListConstructor($ids);
+};

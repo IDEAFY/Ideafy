e2b6c51ee6b9bc4c55078432a5a097cf24bbfdd4
Vincent Weyl <vincent.weyl@taiaut.com>
2014-07-18 17:29:11 -0700

    .

--- public/js-ios/services/config.js
+++ public/js-ios/services/config.js
@@ -11,9 +11,8 @@ define(["Store", "SocketIOTransport", "CouchDBDocument", "Observable"],
         
         this.reset = function(){
                  
-               //_location = "http://8.19.34.68:1664";
-                _location = "http://mobile.ideafy.com:1664"; // uncomment for app deployment
-                _version = "1.2.4";
+               _location = "http://mobile.ideafy.com:1664"; // uncomment for app deployment
+                _version = "1.3.0";
                 _socket = io.connect(_location, {'reconnect': true, 'reconnection delay': 500, 'max reconnection attempts': 10});
                 _transport = new Transport(_socket);
                 _user =  new CouchDBDocument();

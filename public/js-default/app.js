c55205dcc18918e05374bd698e36aa7719064e2b
Vincent Weyl <vincent.weyl@taiaut.com>
2014-08-08 18:26:14 -0700

    .

--- public/js-default/app.js
+++ public/js-default/app.js
@@ -18355,6 +18355,7 @@ module.exports = function ContactsConstructor(){
                         };
                         
                         contactsUI.init = function init(){
+                                ADDGRP = addGroup;
                                 contactList.reset(user.get("connections"));
                                 // show add Contact page by default
                                 console.log("before addcontact init");

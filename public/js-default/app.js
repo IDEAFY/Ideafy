b1054015c0f4e8a25698779481530e45fd25ffdd
Vincent Weyl <vincent.weyl@taiaut.com>
2014-09-22 06:46:33 -0700

    modified:   app.js 	modified:   main.js

--- public/js-default/app.js
+++ public/js-default/app.js
@@ -6139,30 +6139,32 @@ var olives = require("./libs/olives"),
        Confirm = require("./services/confirm"),
        CouchDBChanges = require("./libs/CouchDBTools").CouchDBChanges;
 
-//declaration
-var   _body = new Widget(),
-        _stack = new Stack({}),
-        _dock = new Dock(),
-        _login,
-        _local = new LocalStore(),
-        updateLabels = Utils.updateLabels,
-        checkServerStatus = Utils.checkServerStatus, 
-        _labels = Config.get("labels"), 
-        _db = Config.get("db"), 
-        _transport = Config.get("transport"),
-        _user = Config.get("user"),
-        _currentVersion = Config.get("version");
 
-// SETUP
+(function(){
+        //declaration
+        var   _body = new Widget(),
+                _stack = new Stack({}),
+                _dock = new Dock(),
+                _login,
+                _local = new LocalStore(),
+                updateLabels = Utils.updateLabels,
+                checkServerStatus = Utils.checkServerStatus, 
+                _labels = Config.get("labels"), 
+                _db = Config.get("db"), 
+                _transport = Config.get("transport"),
+                _user = Config.get("user"),
+                _currentVersion = Config.get("version");
 
-// init logic
-_body.startDock = function startDock(firstStart){
+        // SETUP
+
+        // init logic
+        _body.startDock = function startDock(firstStart){
                 document.getElementById("main").classList.add("main");
                 _stack.getStack().show("#dock");
                 _dock.start(firstStart);         
         };
         
-_body.init = function init(firstStart) {
+        _body.init = function init(firstStart) {
                 // add dock UI to the stack
                 _stack.getStack().add("#dock", _dock);
                 // check db
@@ -6221,8 +6223,8 @@ _body.init = function init(firstStart) {
                 });      
         };
         
-// init after exit and re-entry
-_body.reload = function reload(firstStart) {
+        // init after exit and re-entry
+        _body.reload = function reload(firstStart) {
                 _user.unsync();
                 _user.reset();
                 
@@ -6277,141 +6279,141 @@ _body.reload = function reload(firstStart) {
                 });      
         };
        
-// uis declaration
-_dock = new Dock();
-_login = new Login(_body.init, _body.reload, _local);
+        // uis declaration
+        _dock = new Dock();
+        _login = new Login(_body.init, _body.reload, _local);
         
-// add login to the stack
-_stack.getStack().add("#login", _login);
-        
-// Widget definition
-_body.seam.addAll({
-        "stack" : _stack,
-        "place": new Place({confirm: Confirm})
-});
-        
-_body.template = '<div id="main"><div data-stack="destination"></div><div data-place="place:confirm"></div></div>';
-        
-_body.place(document.body);
-        
-// INITIALIZATION
-        
-// retrieve local data
-_local.sync("ideafy-data");
-        
-// stop apploading
- document.getElementById("apploading").classList.add("invisible");
-        
-// display login screen
-_stack.getStack().show("#login");
-_stack.getStack().setCurrentScreen(_login);
-        
-_login.init();
-        
-// check connection
-if (navigator.connection && navigator.connection.type === "none"){
-        // get labels or assign default ones
-        (_local.get("labels")) ? _labels.reset(_local.get("labels")) : _labels.reset(Config.get("defaultLabels"));
-        Config.set("lang", _labels.set("language"));
-        _login.setScreen("#nointernet");
-}
-else {
-        checkServerStatus()
-        .then(function(){
-        
-                // initialize labels to device language if available or US by default
-                if (_local.get("labels")) {
-                        _labels.reset(_local.get("labels"));
-                        Config.set("lang", _labels.get("language"));
-                }
-                else{
-                        updateLabels(navigator.language);
-                }
-                        
-                // check client version
-                _transport.request("CheckVersion", {version: _currentVersion}, function(result){
-                        console.log(result);
-                        var msg = _labels.get("outdated") || "Please update your application";
-                        if (result === "outdated"){
-                                alert(msg);
-                        }        
-                });
-
-                var current = _local.get("currentLogin") || "";
-                
-                // if the last user is in the local storage show login if not display signup screen
-                if (!current) {
-                        //display signup
-                        _login.setScreen("#signup-screen");
-                }
-                else {
-                        _transport.request("CheckLogin", {"id" : current, "sock" : Config.get("socket").socket.sessionid}, function(result) {
-                                console.log(result);
-                                if (result.authenticated) _body.init();
-                                else {
-                                        _login.setScreen("#login-screen");
-                                }
-                        });
-                }
-        }, function(error){
-                (_local.get("labels")) ? _labels.reset(_local.get("labels")) : _labels.reset(Config.get("defaultLabels"));
-                _login.setScreen("#maintenance-screen");
-        });
-}
-         
-/*
-* Watch for signout events
-*/       
-Config.get("observer").watch("signout", function(){
-        
-        // disconnect socket (will change presence status)
-        Config.get("socket").disconnect();
-        // clear local store
-        _local.set("currentLogin", "");
-        _local.set("userAvatar", "");
-        _local.sync("ideafy-data");
-                
-        // reset login UI
+        // add login to the stack
         _stack.getStack().add("#login", _login);
-        _login.reset(true);
-                
-        // hide dock and cache
-        _body.dom.classList.remove("main");
-        document.getElementById("cache").classList.remove("appear");
-                
-        // show loogin screen
+        
+        // Widget definition
+        _body.seam.addAll({
+                "stack" : _stack,
+                "place": new Place({confirm: Confirm})
+        });
+        
+        _body.template = '<div id="main"><div data-stack="destination"></div><div data-place="place:confirm"></div></div>';
+        
+        _body.place(document.body);
+        
+        // INITIALIZATION
+        
+        // retrieve local data
+        _local.sync("ideafy-data");
+        
+        // stop apploading
+        document.getElementById("apploading").classList.add("invisible");
+        
+        // display login screen
         _stack.getStack().show("#login");
         _stack.getStack().setCurrentScreen(_login);
-        _login.setScreen("#login-screen");
-});
         
-// attempt to reconnect socket if required in case of user actions
-Map.get("body").addEventListener("mousedown", Utils.checkSocketStatus, false);
+        _login.init();
         
-// resync user document upon socket reconnection
-Config.get("observer").watch("reconnect", function(option){
-        _local.sync("ideafy-data");
-        if (option === "all"){
+        // check connection
+        if (navigator.connection && navigator.connection.type === "none"){
+                // get labels or assign default ones
+                (_local.get("labels")) ? _labels.reset(_local.get("labels")) : _labels.reset(Config.get("defaultLabels"));
+                Config.set("lang", _labels.set("language"));
+                _login.setScreen("#nointernet");
+        }
+        else {
                 checkServerStatus()
                 .then(function(){
-                        _user.unsync();
-                        CouchDBChanges.initStream(_db);
-                        return _user.sync(_db, _local.get("currentLogin"));
-                }, function(){
-                        _login.setScreen("#maintenance-screen");        
-                })
-                .then(function(){
+                        // initialize labels to device language if available or US by default
+                        if (_local.get("labels")) {
+                                _labels.reset(_local.get("labels"));
+                                Config.set("lang", _labels.get("language"));
+                        }
+                        else{
+                                updateLabels(navigator.language);
+                        }
+                        
+                        // check client version
+                        _transport.request("CheckVersion", {version: _currentVersion}, function(result){
+                                console.log(result);
+                                var msg = _labels.get("outdated") || "Please update your application";
+                                if (result === "outdated"){
+                                        alert(msg);
+                                }        
+                        });
+
+                        var current = _local.get("currentLogin") || "";
+                
+                        // if the last user is in the local storage show login if not display signup screen
+                        if (!current) {
+                                //display signup
+                                _login.setScreen("#signup-screen");
+                        }
+                        else {
+                                _transport.request("CheckLogin", {"id" : current, "sock" : Config.get("socket").socket.sessionid}, function(result) {
+                                        console.log(result);
+                                        if (result.authenticated) _body.init();
+                                        else {
+                                                _login.setScreen("#login-screen");
+                                        }
+                                });
+                        }
+                }, function(error){
+                        (_local.get("labels")) ? _labels.reset(_local.get("labels")) : _labels.reset(Config.get("defaultLabels"));
+                        _login.setScreen("#maintenance-screen");
+                });
+        }
+         
+        /*
+        * Watch for signout events
+        */       
+        Config.get("observer").watch("signout", function(){
+        
+                // disconnect socket (will change presence status)
+                Config.get("socket").disconnect();
+                // clear local store
+                _local.set("currentLogin", "");
+                _local.set("userAvatar", "");
+                _local.sync("ideafy-data");
+                
+                // reset login UI
+                _stack.getStack().add("#login", _login);
+                _login.reset(true);
+                
+                // hide dock and cache
+                _body.dom.classList.remove("main");
+                document.getElementById("cache").classList.remove("appear");
+                
+                // show loogin screen
+                _stack.getStack().show("#login");
+                _stack.getStack().setCurrentScreen(_login);
+                _login.setScreen("#login-screen");
+        });
+        
+        // attempt to reconnect socket if required in case of user actions
+        Map.get("body").addEventListener("mousedown", Utils.checkSocketStatus, false);
+        
+        // resync user document upon socket reconnection
+        Config.get("observer").watch("reconnect", function(option){
+                _local.sync("ideafy-data");
+                if (option === "all"){
+                        checkServerStatus()
+                        .then(function(){
+                                _user.unsync();
+                                CouchDBChanges.initStream(_db);
+                                return _user.sync(_db, _local.get("currentLogin"));
+                        }, function(){
+                                _login.setScreen("#maintenance-screen");        
+                        })
+                        .then(function(){
+                                _user.set("online", true);
+                                _user.set("sock", Config.get("socket").socket.sessionid);
+                                return _user.upload();
+                        });
+                }
+                else{
                         _user.set("online", true);
                         _user.set("sock", Config.get("socket").socket.sessionid);
                         return _user.upload();
-                });
-        }
-        else{
-                _user.set("online", true);
-                _user.set("sock", Config.get("socket").socket.sessionid);
-                return _user.upload();
-        }              
-});
+                }              
+        });
+})();
 
 },{"./libs/CouchDBTools":72,"./libs/amy2":75,"./libs/emily":97,"./libs/olives":141,"./packages/dock":205,"./packages/login":226,"./services/config":245,"./services/confirm":246,"./services/map":249,"./services/utils":256}],145:[function(require,module,exports){
 /*
